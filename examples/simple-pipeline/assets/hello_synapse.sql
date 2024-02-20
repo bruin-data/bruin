034414f7dcd264e6e7aa6bd10ea090a65bdcbbd3
Alberto J. Gomez <alberto.gomez@getbruin.com>
2024-02-20 15:43:27 +0100

    Add synapse example Use `synapse.sql` instead of `sy.sql`

---
+++ examples/simple-pipeline/assets/hello_synapse.sql
@@ -0,0 +1,43 @@
+/* @bruin
+
+name: hello_synapse_test
+type: synapse.sql
+
+materialization:
+   type: table
+
+columns:
+  - name: id
+    type: integer
+    description: "Just a number"
+    primary_key: true
+    checks:
+        - name: not_null
+        - name: positive
+        - name: non_negative
+
+  - name: country
+    type: varchar
+    description: "the country"
+    primary_key: true
+    checks:
+        - name: not_null
+
+  - name: name
+    type: varchar
+    update_on_merge: true
+    description: "Just a name"
+    checks:
+        - name: unique
+        - name: not_null
+
+
+@bruin */
+
+SELECT 1 as id, 'spain' as country , 'juan' as name
+union all
+SELECT 2 as id, 'germany' as country , 'markus' as name
+union all
+SELECT 3 as id, 'france' as country , 'antoine' as name
+union all
+SELECT 4 as id, 'poland' as country , 'maciej' as name
