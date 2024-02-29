3f4930f7ed99e9451bcbbf68de54916972a0574a
Alberto J. Gomez <alberto.gomez@getbruin.com>
2024-02-29 14:43:58 +0100

    generate pipeline

--- cmd/init.go
+++ cmd/init.go
@@ -1,10 +1,13 @@
 package cmd
 
 import (
+	"github.com/bruin-data/bruin/pkg/pipeline"
 	"github.com/urfave/cli/v2"
+	"gopkg.in/yaml.v3"
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 func Init(isDebug *bool) *cli.Command {
@@ -23,7 +26,6 @@ func Init(isDebug *bool) *cli.Command {
 				}
 			}()
 
-			inputPath := c.Args().Get(0)
 			inputPath := c.Args().Get(0)
 			if inputPath == "" {
 				errorPrinter.Printf("Please provide a name for bruin to create a folder where the pipeline will be created: bruin init <name of folder to be created>)\n")
@@ -44,6 +46,47 @@ func Init(isDebug *bool) *cli.Command {
 			err := os.Mkdir(inputPath, 0755)
 			if err != nil {
 				errorPrinter.Printf("Failed to create the folder %s: %v\n", inputPath, err)
+				return cli.Exit("", 1)
+			}
+
+			err = os.Mkdir(filepath.Join(inputPath, "assets"), 0755)
+			if err != nil {
+				errorPrinter.Printf("Failed to create the folder %s: %v\n", filepath.Join(inputPath, "assets"), err)
+				return cli.Exit("", 1)
+			}
+
+			err = os.WriteFile(filepath.Join(inputPath, ".gitignore"), []byte(".bruin.yml\n"), 0644)
+			if err != nil {
+				errorPrinter.Printf("Could not create .gitignore file\n, %v", err)
+				return cli.Exit("", 1)
+			}
+
+			pipeline := pipeline.Pipeline{
+				LegacyID:          "",
+				Name:              inputPath,
+				Schedule:          "daily",
+				StartDate:         time.Now().Format("2006-01-02"),
+				DefinitionFile:    pipeline.DefinitionFile{},
+				DefaultParameters: nil,
+				DefaultConnections: map[string]string{
+					"postgres": "example-postgres-connection",
+				},
+				Assets:        nil,
+				Notifications: pipeline.Notifications{},
+				Catchup:       false,
+				Retries:       0,
+				TasksByType:   nil,
+			}
+
+			yamlData, err := yaml.Marshal(&pipeline)
+			if err != nil {
+				errorPrinter.Printf("Failed to marshal the pipeline definition to yaml: %v\n", err)
+				return cli.Exit("", 1)
+			}
+
+			err = os.WriteFile(filepath.Join(inputPath, "pipeline.yml"), yamlData, 0644)
+			if err != nil {
+				errorPrinter.Printf("Failed to create the pipeline.yml file: %v\n", err)
 				return cli.Exit("", 1)
 			}
 
