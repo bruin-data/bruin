be7203c24f823bba8c13d8f014237db04c512934
Alberto J. Gomez <alberto.gomez@getbruin.com>
2024-03-15 16:29:49 +0100

    add test for mssql ingestr url

--- pkg/mssql/config_test.go
+++ pkg/mssql/config_test.go
@@ -18,3 +18,16 @@ func TestConfig_ToDBConnectionURI(t *testing.T) {
 
 	assert.Equal(t, "sqlserver://user:password@localhost:1433?app+name=Bruin+CLI&database=database", c.ToDBConnectionURI())
 }
+
+func TestConfig_ToIngestr(t *testing.T) {
+	t.Parallel()
+	c := Config{
+		Username: "user",
+		Password: "password",
+		Host:     "localhost",
+		Port:     1433,
+		Database: "database",
+	}
+
+	assert.Equal(t, "mssql://user:password@localhost:1433/database", c.ToIngestrURL())
+}
