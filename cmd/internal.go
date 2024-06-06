ae2be5151017b40d02870681ebb3e13aa0c38cc7
Burak Karakan <burak.karakan@gmail.com>
2024-06-07 00:43:01 +0300

    add an internal command

--- cmd/internal.go
+++ cmd/internal.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/bruin-data/bruin/pkg/sqlparser"
 
 	"github.com/bruin-data/bruin/pkg/git"
 	"github.com/bruin-data/bruin/pkg/path"
@@ -17,6 +18,7 @@ func Internal() *cli.Command {
 		Hidden: true,
 		Subcommands: []*cli.Command{
 			ParseAsset(),
+			SqlParseTest(),
 		},
 	}
 }
@@ -33,6 +35,68 @@ func ParseAsset() *cli.Command {
 			}
 
 			return r.Run(c.Args().Get(0))
+		},
+	}
+}
+
+func SqlParseTest() *cli.Command {
+	return &cli.Command{
+		Name: "sqlparse-test",
+		Action: func(c *cli.Context) error {
+			p, err := sqlparser.NewSqlParser()
+			if err != nil {
+				errorPrinter.Printf("failed to initialize sqlparser module: %+v\n", err)
+				return cli.Exit("", 1)
+			}
+
+			err = p.Start()
+			if err != nil {
+				errorPrinter.Printf("failed to start sqlparser module: %+v\n", err)
+				return cli.Exit("", 1)
+			}
+
+			query := `SELECT
+					sales.id,
+					CASE
+						WHEN sales.amount > 500 THEN 'large'
+						WHEN sales.amount > 100 THEN 'medium'
+						ELSE 'small'
+					END as sale_size,
+					CASE
+						WHEN regions.name = 'North' THEN 'N'
+						WHEN regions.name = 'South' THEN 'S'
+						ELSE 'Other'
+					END as region_abbr
+				FROM sales
+				JOIN regions ON sales.region_id = regions.id`
+
+			schema := sqlparser.Schema{
+				"sales":   {"id": "str", "amount": "int64", "region_id": "str"},
+				"regions": {"id": "str", "name": "str"},
+			}
+
+			res, err := p.ColumnLineage(query, "bigquery", schema)
+			if err != nil {
+				errorPrinter.Printf("failed to get column lineage: %+v\n", err)
+				return cli.Exit("", 1)
+			}
+
+			js, err := json.Marshal(res)
+			if err != nil {
+				errorPrinter.Printf("failed to marshal result: %+v\n", err)
+				return cli.Exit("", 1)
+			}
+
+			expected := "{\"columns\":[{\"name\":\"id\",\"upstream\":[{\"column\":\"id\",\"table\":\"sales\"}]},{\"name\":\"region_abbr\",\"upstream\":[{\"column\":\"name\",\"table\":\"regions\"}]},{\"name\":\"sale_size\",\"upstream\":[{\"column\":\"amount\",\"table\":\"sales\"}]}]}"
+			if string(js) != expected {
+				errorPrinter.Printf("expected: %s, got: %s\n", expected, string(js))
+				return cli.Exit("", 1)
+			}
+
+			fmt.Println()
+			successPrinter.Println("Success, it seems like the query parsing has worked! Please let Burak know that it worked.")
+			fmt.Println()
+			return nil
 		},
 	}
 }
