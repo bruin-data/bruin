19ab92151008a1f1ee8ad83a45910c95b08b197e
Alberto J. Gomez <alberto.gomez@getbruin.com>
2024-08-09 10:57:08 +0200

    add schema and catalog to databricks

--- pkg/databricks/config.go
+++ pkg/databricks/config.go
@@ -2,17 +2,37 @@ package databricks
 
 import (
 	"fmt"
+	"net/url"
 	"strings"
 )
 
 type Config struct {
+	// Token is the Databricks Personal API token
 	Token string
 	Host  string
 	Port  int
 	// Path will determine cluster or sql warehouse. See https://docs.databricks.com/en/dev-tools/go-sql-driver.html#connect-with-a-dsn-connection-string
-	Path string
+	Path    string
+	Catalog string
+	Schema  string
 }
 
 func (c *Config) ToDBConnectionURI() string {
-	return fmt.Sprintf("token:%s@%s:%d/%s", c.Token, c.Host, c.Port, strings.TrimPrefix(c.Path, "/"))
+	query := url.Values{}
+
+	if c.Catalog != "" {
+		query.Add("catalog", c.Catalog)
+	}
+	if c.Schema != "" {
+		query.Add("schema", c.Schema)
+
+	}
+	dsn := url.URL{
+		User:     url.UserPassword("token", c.Token),
+		Host:     fmt.Sprintf("%s:%d", c.Host, c.Port),
+		RawQuery: query.Encode(),
+		Path:     c.Path,
+	}
+
+	return strings.TrimPrefix(dsn.String(), "//")
 }
--- pkg/databricks/config_test.go
+++ pkg/databricks/config_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestConfig_ToIngestr(t *testing.T) {
+func TestConfig_ToDSNNoQuery(t *testing.T) {
 	t.Parallel()
 	c := Config{
 		Host:  "localhost",
@@ -16,6 +16,22 @@ func TestConfig_ToIngestr(t *testing.T) {
 	}
 
 	expected := "token:xxxxxx@localhost:443/sql/1.0/endpoints/a1b234c5678901d2"
+
+	assert.Equal(t, expected, c.ToDBConnectionURI())
+}
+
+func TestConfig_ToDSN(t *testing.T) {
+	t.Parallel()
+	c := Config{
+		Host:    "azuredatabricks.com",
+		Token:   "yyyyy",
+		Path:    "sql/1.0/endpoints/a1b234c5678901d2",
+		Port:    444,
+		Catalog: "my_catalog",
+		Schema:  "my_schema",
+	}
+
+	expected := "token:yyyyy@azuredatabricks.com:444/sql/1.0/endpoints/a1b234c5678901d2?catalog=my_catalog&schema=my_schema"
 
 	assert.Equal(t, expected, c.ToDBConnectionURI())
 }
