ac9195592ec83f71b9f7d9504a61fc3a3d69fd3e
Sanju <sanjushahgupta@gmail.com>
2024-10-10 19:25:50 +0200

    add appsflyer

--- pkg/config/manager.go
+++ pkg/config/manager.go
@@ -211,6 +211,15 @@ func (c FacebookAdsConnection) GetName() string {
 	return c.Name
 }
 
+type AppsflyerConnection struct {
+	Name   string `yaml:"name" json:"name" mapstructure:"name"`
+	ApiKey string `yaml:"api_key" json:"api_key" mapstructure:"api_key"`
+}
+
+func (c AppsflyerConnection) GetName() string {
+	return c.Name
+}
+
 type MySQLConnection struct {
 	Name     string `yaml:"name" json:"name" mapstructure:"name"`
 	Username string `yaml:"username" json:"username" mapstructure:"username"`
@@ -314,6 +323,7 @@ type Connections struct {
 	Generic             []GenericConnection             `yaml:"generic,omitempty" json:"generic,omitempty" mapstructure:"generic"`
 	FacebookAds         []FacebookAdsConnection         `yaml:"facebookads,omitempty" json:"facebookads,omitempty" mapstructure:"facebookads"`
 	Stripe              []StripeConnection              `yaml:"stripe,omitempty" json:"stripe,omitempty" mapstructure:"stripe"`
+	Appsflyer           []AppsflyerConnection           `yaml:"appsflyer,omitempty" json:"appsflyer,omitempty" mapstructure:"appsflyer"`
 
 	byKey       map[string]any
 	typeNameMap map[string]string
@@ -433,6 +443,11 @@ func (c *Connections) buildConnectionKeyMap() {
 	for i, conn := range c.Stripe {
 		c.byKey[conn.Name] = &(c.Stripe[i])
 		c.typeNameMap[conn.Name] = "stripe"
+	}
+
+	for i, conn := range c.Appsflyer {
+		c.byKey[conn.Name] = &(c.Appsflyer[i])
+		c.typeNameMap[conn.Name] = "appsflyer"
 	}
 }
 
@@ -707,6 +722,13 @@ func (c *Config) AddConnection(environmentName, name, connType string, creds map
 		}
 		conn.Name = name
 		env.Connections.FacebookAds = append(env.Connections.FacebookAds, conn)
+	case "appsflyer":
+		var conn AppsflyerConnection
+		if err := mapstructure.Decode(creds, &conn); err != nil {
+			return fmt.Errorf("failed to decode credentials: %w", err)
+		}
+		conn.Name = name
+		env.Connections.Appsflyer = append(env.Connections.Appsflyer, conn)
 	default:
 		return fmt.Errorf("unsupported connection type: %s", connType)
 	}
@@ -778,6 +800,8 @@ func (c *Config) DeleteConnection(environmentName, connectionName string) error 
 		env.Connections.FacebookAds = removeConnection(env.Connections.FacebookAds, connectionName)
 	case "stripe":
 		env.Connections.Stripe = removeConnection(env.Connections.Stripe, connectionName)
+	case "appsflyer":
+		env.Connections.Appsflyer = removeConnection(env.Connections.Appsflyer, connectionName)
 	default:
 		return fmt.Errorf("unsupported connection type: %s", connType)
 	}
