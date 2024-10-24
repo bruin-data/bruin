2deb1a5bca79088e77dd3b7baeac1888b99dd383
Yuvraj <yuvraj.yadav@getbruin.com>
2024-10-25 01:55:59 +0530

    validation for json

--- pkg/connection/connection.go
+++ pkg/connection/connection.go
@@ -653,15 +653,26 @@ func (m *Manager) AddBqConnectionFromConfig(connection *config.GoogleCloudPlatfo
 	}
 	m.mutex.Unlock()
 
-	file, err := ioutil.ReadFile(connection.ServiceAccountFile)
-	if err != nil {
-		return err
+	if len(connection.ServiceAccountFile) > 0 && connection.ServiceAccountFile != "" {
+		file, err := ioutil.ReadFile(connection.ServiceAccountFile)
+		if err != nil {
+			return errors.New("failed to read service account file")
+		}
+		var js json.RawMessage
+		err = json.Unmarshal(file, &js)
+		if err != nil {
+			return errors.New("not a valid JSON in service account file")
+		}
 	}
-	var js json.RawMessage
-	err = json.Unmarshal(file, &js)
-	if err != nil {
-		return err
+
+	if len(connection.ServiceAccountJSON) > 0 && connection.ServiceAccountJSON != "" {
+		var js json.RawMessage
+		err := json.Unmarshal([]byte(connection.ServiceAccountJSON), &js)
+		if err != nil {
+			return errors.New("not a valid JSON in service account json")
+		}
 	}
+
 	db, err := bigquery.NewDB(&bigquery.Config{
 		ProjectID:           connection.ProjectID,
 		CredentialsFilePath: connection.ServiceAccountFile,
