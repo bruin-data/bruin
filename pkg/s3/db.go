a5920ef76d68ac27e766edb061396867f32c8182
Sanju <sanjushahgupta@gmail.com>
2024-10-31 14:24:23 +0100

    add test data

---
+++ pkg/s3/db.go
@@ -0,0 +1,19 @@
+package s3
+
+type Client struct {
+	config Config
+}
+
+type GetIngestrURI interface {
+	GetIngestrURI() string
+}
+
+func NewClient(c Config) (*Client, error) {
+	return &Client{
+		config: c,
+	}, nil
+}
+
+func (c *Client) GetIngestrURI() (string, error) {
+	return c.config.GetIngestrURI(), nil
+}
