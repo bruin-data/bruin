4f5be957d1568838c82ba892a102fa7598f79b32
baristerzioglu <baristerzioglu1432@gmail.com>
2024-11-15 09:54:09 +0100

    updated tutorial

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,30 +1,49 @@
-import {defineConfig} from "vitepress";
+import { defineConfig } from "vitepress";
+import { createHighlighter } from "shiki";
+import jinja2Grammar from "shiki/langs/jinja-html.mjs";
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the directory name of the current module
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
+const bruinSqlGrammar = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, "theme/bruinsql.json"), "utf8")
+);
+
+const bruinPythonGrammar = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, "theme/bruinpython.json"), "utf8")
+);
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
     title: "Bruin CLI",
     description: "Open-source multi-language data pipelines",
-    base: '/bruin/',
+    base: "/bruin/",
     themeConfig: {
-        outline: 'deep',
+        outline: "deep",
         search: {
-            provider: 'local'
+            provider: "local",
         },
-        nav: [{text: "Home", link: "/"}],
+        nav: [{ text: "Home", link: "/" }],
         sidebar: [
             {
                 text: "Getting Started",
                 collapsed: false,
                 items: [
                     {
-                        text: "Introduction", link: "/", items: [
-                            {text: "Installation", link: "/getting-started/introduction/installation"},
-                            {text: "Quickstart", link: "/getting-started/introduction/quickstart"},
-                        ]
+                        text: "Introduction",
+                        link: "/",
+                        items: [
+                            { text: "Installation", link: "/getting-started/introduction/installation" },
+                            { text: "Quickstart", link: "/getting-started/introduction/quickstart" },
+                        ],
                     },
-                    {text: "Concepts", link: "/getting-started/concepts"},
-                    {text: "Design Principles", link: "/getting-started/design-principles"},
-                    {text: "Glossary", link: "/getting-started/glossary"},
+                    { text: "Concepts", link: "/getting-started/concepts" },
+                    { text: "Design Principles", link: "/getting-started/design-principles" },
+                    { text: "Glossary", link: "/getting-started/glossary" },
                     {text: "Tutorials", link: "/getting-started/tutorials"},
                 ],
             },
@@ -32,16 +51,15 @@ export default defineConfig({
                 text: "Assets",
                 collapsed: false,
                 items: [
-                    {text: "Definition Schema", link: "/assets/definition-schema"},
-                    {text: "Columns", link: "/assets/columns"},
-                    {text: "Materialization", link: "/assets/materialization"},
-                    {text: "Ingestr", link: "/assets/ingestr"},
-                    {text: "Python Assets", link: "/assets/python"},
+                    { text: "Definition Schema", link: "/assets/definition-schema" },
+                    { text: "Columns", link: "/assets/columns" },
+                    { text: "Materialization", link: "/assets/materialization" },
+                    { text: "Ingestr", link: "/assets/ingestr" },
+                    { text: "Python Assets", link: "/assets/python" },
                     {
-                        text: "Templating", link: "/templating/templating", items: [
-                            {text: "Filters", link: "/templating/filters"},
-
-                        ]
+                        text: "Templating",
+                        link: "/assets/templating/templating",
+                        items: [{ text: "Filters", link: "/assets/templating/filters" }],
                     },
                 ],
             },
@@ -49,41 +67,44 @@ export default defineConfig({
                 text: "Data Platforms",
                 collapsed: false,
                 items: [
-                    {text: "AWS Athena", link: "/platforms/athena"},
-                    {text: "Databricks", link: "/platforms/databricks"},
-                    {text: "DuckDB", link: "/platforms/duckdb"},
-                    {text: "Google BigQuery", link: "/platforms/bigquery"},
-                    {text: "Microsoft SQL Server", link: "/platforms/mssql"},
-                    {text: "Postgres", link: "/platforms/postgres"},
-                    {text: "Redshift", link: "/platforms/redshift"},
-                    {text: "Snowflake", link: "/platforms/snowflake"},
-                    {text: "Synapse", link: "/platforms/synapse"},
+                    { text: "AWS Athena", link: "/platforms/athena" },
+                    { text: "Databricks", link: "/platforms/databricks" },
+                    { text: "DuckDB", link: "/platforms/duckdb" },
+                    { text: "Google BigQuery", link: "/platforms/bigquery" },
+                    { text: "Microsoft SQL Server", link: "/platforms/mssql" },
+                    { text: "Postgres", link: "/platforms/postgres" },
+                    { text: "Redshift", link: "/platforms/redshift" },
+                    { text: "Snowflake", link: "/platforms/snowflake" },
+                    { text: "Synapse", link: "/platforms/synapse" },
                 ],
             },
             {
                 text: "Data Ingestion",
                 collapsed: false,
                 items: [
-                    {text: "Overview", link: "/ingestion/overview"},
+                    { text: "Overview", link: "/ingestion/overview" },
                     {
-                        text: "Sources", collapsed: true, items: [
-                            {text: "Adjust", link: "/ingestion/adjust.md"},
-                            {text: "Airtable", link: "/ingestion/airtable.md"},
-                            {text: "Chess", link: "/ingestion/chess.md"},
-                            {text: "Facebook", link: "/ingestion/facebook-ads.md"},
-                            {text: "Google Sheets", link: "/ingestion/google_sheets.md"},
-                            {text: "Gorgias", link: "/ingestion/gorgias"},
-                            {text: "Hubspot", link: "/ingestion/hubspot.md"},
-                            {text: "Kafka", link: "/ingestion/kafka.md"},
-                            {text: "Klaviyo", link: "/ingestion/klaviyo.md"},
-                            {text: "MongoDB", link: "/ingestion/mongo"},
-                            {text: "MySQL", link: "/ingestion/mysql"},
-                            {text: "Notion", link: "/ingestion/notion"},
-                            {text: "SAP HANA", link: "/ingestion/sap_hana"},
-                            {text: "S3", link: "/ingestion/s3"},
-                            {text: "Slack", link: "/ingestion/slack.md"},
-                            {text: "Zendesk", link: "/ingestion/zendesk.md"},
-                        ]
+                        text: "Sources",
+                        collapsed: true,
+                        items: [
+                            { text: "Adjust", link: "/ingestion/adjust.md" },
+                            { text: "Airtable", link: "/ingestion/airtable.md" },
+                            { text: "Chess", link: "/ingestion/chess.md" },
+                            { text: "Facebook", link: "/ingestion/facebook-ads.md" },
+                            { text: "Google Sheets", link: "/ingestion/google_sheets.md" },
+                            { text: "Gorgias", link: "/ingestion/gorgias" },
+                            { text: "Hubspot", link: "/ingestion/hubspot.md" },
+                            { text: "Kafka", link: "/ingestion/kafka.md" },
+                            { text: "Klaviyo", link: "/ingestion/klaviyo.md" },
+                            { text: "MongoDB", link: "/ingestion/mongo" },
+                            { text: "MySQL", link: "/ingestion/mysql" },
+                            { text: "Notion", link: "/ingestion/notion" },
+                            { text: "SAP HANA", link: "/ingestion/sap_hana" },
+                            { text: "S3", link: "/ingestion/s3" },
+                            { text: "Slack", link: "/ingestion/slack.md" },
+                            { text: "Zendesk", link: "/ingestion/zendesk.md" },
+                            { text: "Shopify", link: "/ingestion/shopify" },
+                        ],
                     },
                 ],
             },
@@ -91,9 +112,9 @@ export default defineConfig({
                 text: "Commands",
                 collapsed: false,
                 items: [
-                    {text: "Init", link: "/commands/init"},
-                    {text: "Connections", link: "/commands/connections.md"},
-                    {text: "Run", link: "/commands/run"},
+                    { text: "Init", link: "/commands/init" },
+                    { text: "Connections", link: "/commands/connections.md" },
+                    { text: "Run", link: "/commands/run" },
                 ],
             },
             {
@@ -101,15 +122,17 @@ export default defineConfig({
                 collapsed: false,
                 items: [
                     {
-                        text: "Overview", link: "/quality/overview", items: [
-                            {text: "Not null", link: "/quality/not_null"},
-                            {text: "Unique", link: "/quality/unique"},
-                            {text: "Positive", link: "/quality/positive"},
-                            {text: "Non Negative", link: "/quality/non_negative"},
-                            {text: "Negative", link: "/quality/negative"},
-                            {text: "Accepted values", link: "/quality/accepted_values"},
-                            {text: "Pattern", link: "/quality/pattern"},
-                        ]
+                        text: "Overview",
+                        link: "/quality/overview",
+                        items: [
+                            { text: "Not null", link: "/quality/not_null" },
+                            { text: "Unique", link: "/quality/unique" },
+                            { text: "Positive", link: "/quality/positive" },
+                            { text: "Non Negative", link: "/quality/non_negative" },
+                            { text: "Negative", link: "/quality/negative" },
+                            { text: "Accepted values", link: "/quality/accepted_values" },
+                            { text: "Pattern", link: "/quality/pattern" },
+                        ],
                     },
                 ],
             },
@@ -118,40 +141,42 @@ export default defineConfig({
                 collapsed: false,
                 items: [
                     {
-                        text: "Overview", link: "/vscode-extension/overview",
+                        text: "Overview",
+                        link: "/vscode-extension/overview",
                         items: [
-                            {text: "Installation", link: "/vscode-extension/installation"},
-                            {text: "Getting Started", link: "/vscode-extension/getting-started"},
-                            {text: "Configuration", link: "/vscode-extension/configuration"},
+                            { text: "Installation", link: "/vscode-extension/installation" },
+                            { text: "Getting Started", link: "/vscode-extension/getting-started" },
+                            { text: "Configuration", link: "/vscode-extension/configuration" },
                             {
                                 text: "Panels",
                                 items: [
-                                    {text: "Side Panel", link: "/vscode-extension/panels/side-panel"},
-                                    {text: "Lineage Panel", link: "/vscode-extension/panels/lineage-panel"}
-                                ]
+                                    { text: "Side Panel", link: "/vscode-extension/panels/side-panel" },
+                                    { text: "Lineage Panel", link: "/vscode-extension/panels/lineage-panel" },
+                                ],
                             },
-                        ]
+                        ],
                     },
-                ]
+                ],
             },
             {
                 text: "CI/CD Integration",
                 collapsed: false,
-                items: [
-                    {text: "Github Actions", link: "/cicd/github-action"},
-                ],
+                items: [{ text: "Github Actions", link: "/cicd/github-action" }],
             },
             {
                 text: "Bruin Cloud",
                 collapsed: false,
                 items: [
-                    {text: "Cross-pipeline dependencies", link: "/cloud/cross-pipeline"},
-                    {text: "Notifications", link: "/cloud/notifications"},
+                    { text: "Cross-pipeline dependencies", link: "/cloud/cross-pipeline" },
+                    { text: "Notifications", link: "/cloud/notifications" },
                 ],
             },
-
         ],
 
-        socialLinks: [{icon: "github", link: "https://github.com/bruin-data/bruin"}],
+        socialLinks: [{ icon: "github", link: "https://github.com/bruin-data/bruin" }],
+    },
+
+    markdown: {
+        languages: ["sql", "yaml", "shell", "python", "json", jinja2Grammar, bruinSqlGrammar, bruinPythonGrammar],
     },
 });
--- docs/getting-started/tutorials.md
+++ docs/getting-started/tutorials.md
@@ -3,15 +3,13 @@
 In this guide, you'll learn how to create a simple pipeline that ingests chess data and stores it in DuckDB using Bruin.
 
 ## Prerequisites
-Make sure you have the following:
-- **Bruin CLI** installed. [Get instructions](https://link-to-bruin-installation).
-- **DuckDB** installed. [Installation guide](https://duckdb.org).
-- **Docker** installed. [Get Docker](https://www.docker.com/get-started).
+Before you start, make sure you have the Bruin CLI installed. [Get instructions](https://link-to-bruin-installation).
+
 
 
 ## Step 1: Initialize Your Pipeline Project
 
-Run the `bruin init chess` command to set up your Bruin project using the `chess` template. 
+Run the `bruin init chess` command to set up your Bruin project using the `chess` template.
 This template brings in the necessary project structure and configuration files for a pipeline specific to chess data.
 
 ```bash 
@@ -22,15 +20,17 @@ After running the command you will see the following folder appear on your proje
 ```plaintext
 chess/
 ├─ assets/
-│  ├─ .chess_ingestion.asset.yml
+│  ├─ chess_games.asset.yml
+│  ├─ chess_profiles.asset.yml
+│  ├─ player_summary.sql
 │
 ├─ .bruin.yml
-├─pipeline.yml  
+├─ pipeline.yml  
 ├─ .gitignore
 └─ README.md
 ```
 
-## Step 2: Edit Your `.bruin.yml` File
+## Step 2: Edit Your `.bruin.yml` file
 After initializing your project with `bruin init`, edit the `.bruin.yml` file to configure your environments and connections. This file specifies the default environment settings and connections your pipeline will use.
 
 Add or modify the `.bruin.yml` file as follows:
@@ -50,30 +50,27 @@ environments:
                       - "MagnusCarlsen"
                       - "Hikaru"
 ```
-## Step 3: Take a look at your `chess_ingestion.asset.yml` File
-Since we initialized our project with the `chess template`, the chess_ingestion.asset.yml is already included in the assets folder,
-pre-configured for chess data ingestion. Open this file to confirm that it’s set up as follows:
-### Example `chess_ingestion.asset.yml`:
-```yaml
-name: raw.chess_ingestion
-type: ingestr
-parameters:
-  source_connection: chess_connection
-  source_table: profiles
-  destination: duckdb
-```
+## Step 3: Take a look at your assets
+Since we initialized our project with the `chess template`, the necessary assets are already included in the assets folder,
+pre-configured for chess data ingestion. Open this file to take a look at your assets, you should see :
+
+- **`chess_games.asset.yml`**: Ingests chess game data from the source connection into DuckDB.
+- **`chess_profiles.asset.yml`**: Ingests player profile data from the source connection into DuckDB.
+- **`player_summary.sql`**: A SQL asset that creates a summary table of player statistics, aggregating game results, wins, and win rates for each player based on the ingested data.
+> [!INFO]
 > **What is an Asset?**  
 > An asset in Bruin is a configuration that details specific data processing tasks, such as ingestion, transformation. Learn more about [Bruin assets](../assets/definition-schema.md).
 
-## Step 4: Check Your `pipeline.yml` File
+## Step 4: Check Your `pipeline.yml` file
 Just like in Step 3, the pipeline.yml file also comes pre-configured for our task, and it follows this structure:
 ### Example `pipeline.yml`:
 ```yaml
-name: chess_duckdb_pipeline
+name: chess_duckdb
 default_connections:
     duckdb: "duckdb_default"  
     chess: "chess_connection"
 ```
+> [!INFO]
 > **What is a Pipeline?**  
 >A pipeline is a group of assets that are executed together in the right order.  Learn more about [Bruin Pipelines](concepts.md#pipeline).
 ## Step 5: Run Your Pipeline and Ingest the Data
@@ -86,13 +83,9 @@ bruin run ./chess/pipeline.yml
 ## Step 6: Query Your Data in DuckDB
 Now that the data is in DuckDB, you can query it to verify the results. Open a terminal and run the following commands to inspect your data:
 
-1. Start DuckDB with this command:
+
 ```bash
-duckdb /path/to/your/database.db
-```
-2. Once inside DuckDB, use SQL to view the ingested data:
-```bash
-   SELECT * FROM chess_games LIMIT 10;
+bruin fetch query --connection duckdb_default  --query "SELECT * FROM chess_playground.player_summary LIMIT 10;"
 ```
 
 Congratulations!
