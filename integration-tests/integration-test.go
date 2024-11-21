6acc54017c2c4155f9fd7f0724903b9ad931b3bd
Alberto J. Gomez <alberto.gomez@getbruin.com>
2024-11-21 16:50:21 +0100

    ignore duckdb.db, add all tests

--- integration-tests/integration-test.go
+++ integration-tests/integration-test.go
@@ -5,139 +5,115 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"syscall"
 
 	jd "github.com/josephburnett/jd/lib"
 )
 
+var currentFolder string
+
 func main() {
-	currentFolder, err := os.Getwd()
+	var err error
+	currentFolder = filepath.Join(currentFolder, "integration-tests")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	integrationTestsFolder := filepath.Join(currentFolder, "integration-tests")
+	expectExitCode("validate happy-path", 0)
+	expectExitCode("run --use-uv happy-path", 0)
+	expectJsonOutput("internal parse-pipeline happy-path", "happy-path/expectations/pipeline.yml.json")
+	expectJsonOutput(
+		"internal parse-asset happy-path/assets/asset.py",
+		"happy-path/expectations/asset.py.json",
+	)
+	expectJsonOutput(
+		"internal parse-asset happy-path/assets/chess_games.asset.yml",
+		"happy-path/expectations/chess_games.asset.yml.json",
+	)
+	expectJsonOutput(
+		"internal parse-asset happy-path/assets/chess_profiles.asset.yml",
+		"happy-path/expectations/chess_profiles.asset.yml.json",
+	)
+	expectJsonOutput(
+		"internal parse-asset happy-path/assets/player_summary.sql",
+		"happy-path/expectations/player_summary.sql.json",
+	)
 
-	entries, err := os.ReadDir(integrationTestsFolder)
-
-	if err != nil {
-		fmt.Printf("failed to read directory %s: %s\n", integrationTestsFolder, err.Error())
-		os.Exit(2)
-	}
-
-	// Iterate over entries
-	for _, entry := range entries {
-		if !entry.IsDir() || entry.Name() == ".git" || entry.Name() == "logs" {
-			continue
-		}
-		// Check if the entry is a directory
-		runTest(entry.Name(), integrationTestsFolder)
-	}
 }
 
-func runTest(testName, integrationTestsFolder string) {
-	folder := filepath.Join(integrationTestsFolder, testName)
-
-	fmt.Println("Running test for:", folder)
-
-	cmd := exec.Command("go", "run", "main.go", "validate", folder)
-	stdout, err := cmd.Output()
-	fmt.Println(string(stdout))
+func expectJsonOutput(command string, jsonFilePath string) {
+	output, err := runCommand(command)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(3)
+		fmt.Println("Failed:", err)
+		os.Exit(1)
 	}
 
-	cmd = exec.Command("go", "run", "main.go", "run", "--use-uv", folder)
-	stdout, err = cmd.Output()
-	fmt.Println(string(stdout))
+	expectation, err := jd.ReadJsonFile(filepath.Join(currentFolder, jsonFilePath))
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(4)
+		fmt.Println("Failed to read expectation:", err)
+		os.Exit(1)
 	}
 
-	cmd = exec.Command("go", "run", "main.go", "internal", "parse-pipeline", folder)
-	stdout, err = cmd.Output()
+	// normalize linebreaks
+	parsedOutput, err := jd.ReadJsonString(strings.ReplaceAll(output, "\\r\\n", "\\n"))
 	if err != nil {
-		fmt.Println("Error running parse-pipeline")
-		fmt.Println(err)
-		os.Exit(4)
-	}
-	expectation, err := jd.ReadJsonFile(filepath.Join(folder, "expectations", "pipeline.yml.json"))
-	if err != nil {
-		fmt.Println("Error running expectation for parse-pipeline")
-		fmt.Println(err)
-		os.Exit(5)
+		fmt.Println("Failed to parse output:", err)
+		os.Exit(1)
 	}
 
-	parsed, err := jd.ReadJsonString(strings.ReplaceAll(string(stdout), "\\r\\n", "\\n"))
-	if err != nil {
-		fmt.Println("Error parsing json output for pipeline " + folder)
-		fmt.Println(err)
-		os.Exit(5)
-	}
-
-	diff := expectation.Diff(parsed)
+	diff := expectation.Diff(parsedOutput)
 	if len(diff) != 0 {
 		var path jd.JsonNode
 		for _, d := range diff {
+			// Paths are hell to normalize, we skip
 			path = d.Path[len(d.Path)-1]
 			if path.Json() == "\"path\"" {
 				continue
 			}
 
-			fmt.Println("Parsed pipeline not matching, last path:")
-			fmt.Println(path.Json())
-			fmt.Println(diff.Render())
-			os.Exit(6)
+			fmt.Print("Expected json: ")
+			fmt.Println(d.NewValues)
+			fmt.Print("Not Matching found json: ")
+			fmt.Println(d.OldValues)
+			os.Exit(1)
 		}
 	}
+	fmt.Println("Passed")
+}
 
-	assets, err := os.ReadDir(filepath.Join(folder, "assets"))
+func expectExitCode(command string, code int) {
+	output, err := runCommand(command)
 	if err != nil {
-		fmt.Println("Error reading assets folder")
-		fmt.Println(err)
-		os.Exit(5)
-	}
-	for _, asset := range assets {
-		if asset.IsDir() {
-			continue
-		}
-		fmt.Println("Checking expectations for:" + asset.Name())
-		cmd = exec.Command("go", "run", "main.go", "internal", "parse-asset", filepath.Join(folder, "assets", asset.Name())) //nolint:gosec
-		stdout, err = cmd.Output()
-		if err != nil {
-			fmt.Println("Error running parse asset")
-			fmt.Println(err)
-			os.Exit(7)
-		}
-
-		expectation, err = jd.ReadJsonFile(filepath.Join(folder, "expectations", asset.Name()) + ".json")
-		if err != nil {
-			fmt.Println("Error reading expectation for parse asset")
-			fmt.Println(err)
-			os.Exit(8)
-		}
-
-		parsed, err = jd.ReadJsonString(strings.ReplaceAll(string(stdout), "\\r\\n", "\\n"))
-		if err != nil {
-			fmt.Println("Error parsing json output for asset " + asset.Name())
-			fmt.Println(err)
-			os.Exit(8)
-		}
-		diff = expectation.Diff(parsed)
-		if len(diff) != 0 {
-			for _, d := range diff {
-				path := d.Path[len(d.Path)-1]
-				if path.Json() == "\"path\"" {
-					continue
+		// Try to get the exit code
+		if exitError, ok := err.(*exec.ExitError); ok {
+			// Get the status code
+			if status, ok := exitError.Sys().(syscall.WaitStatus); ok {
+				if status.ExitStatus() != code {
+					fmt.Println(strconv.Itoa(code) + " Was expected but got:" + strconv.Itoa(status.ExitStatus()))
+					fmt.Println(output)
+					os.Exit(1)
 				}
-				fmt.Printf("Asset %s not matching\n", asset.Name())
-				fmt.Println(path.Json())
-				fmt.Println(diff.Render())
-				os.Exit(6)
 			}
 		}
+		// Handle other errors
+		fmt.Printf("Error running command: %v\n", err)
+		fmt.Println(output)
+		return
 	}
+
+	fmt.Println("Passed")
+}
+
+func runCommand(command string) (string, error) {
+	fmt.Println("Running command:", command)
+	args := []string{"run", "../main.go"}
+	args = append(args, strings.Split(command, " ")...)
+	cmd := exec.Command("go", args...)
+	cmd.Dir = currentFolder
+	output, err := cmd.Output()
+
+	return string(output), err
 }
