e56b88a7f29d91e43a7bba0b9d87f72be3410c4a
y-bruin <yuvraj.yadav@getbruin.com>
2024-12-18 18:53:48 +0530

    more changes

--- pkg/state/state.go
+++ pkg/state/state.go
@@ -10,22 +10,14 @@ import (
 
 var version = "dev"
 
-const (
-	StatusPending Status = "pending"
-	StatusRunning Status = "running"
-	StatusSuccess Status = "success"
-	StatusFailed  Status = "failed"
-	StatusSkipped Status = "skipped"
-)
-
 type State struct {
 	sync.RWMutex
-	Parameters map[string]string                     `json:"parameters"`
-	Metadata   Metadata                              `json:"metadata"`
-	State      map[string][]*scheduler.AssetInstance `json:"state"`
-	Version    string                                `json:"version"`
-	TimeStamp  time.Time                             `json:"timestamp"`
-	RunID      string                                `json:"run_id"`
+	Parameters map[string]string          `json:"parameters"`
+	Metadata   Metadata                   `json:"metadata"`
+	State      []*scheduler.AssetInstance `json:"state"`
+	Version    string                     `json:"version"`
+	TimeStamp  time.Time                  `json:"timestamp"`
+	RunID      string                     `json:"run_id"`
 }
 
 type Metadata struct {
@@ -42,61 +34,9 @@ func NewState(runid string, parameters map[string]string) *State {
 			Version: version,
 			OS:      runtime.GOOS,
 		},
-		State:     map[string][]*scheduler.AssetInstance{},
+		State:     []*scheduler.AssetInstance{},
 		Version:   "1.0.0",
 		TimeStamp: time.Time{},
 		RunID:     runid,
 	}
-}
-
-func (s *State) GetAssetState(name string, pipelineName string) *scheduler.AssetInstance {
-	s.RLock()
-	defer s.RUnlock()
-	for foundPipelineName, states := range s.State {
-		if foundPipelineName == pipelineName {
-			for _, state := range states {
-				if state.Asset.Name == name {
-					return state
-				}
-			}
-		}
-	}
-	return nil
-}
-
-func (s *State) GetPipelineState(name string) []*scheduler.AssetInstance {
-	s.RLock()
-	defer s.RUnlock()
-	for foundPipelineName, states := range s.State {
-		if foundPipelineName == name {
-			return states
-		}
-	}
-	return []*scheduler.AssetInstance{}
-}
-
-func (s *State) SetAssetState(name, pipelineName string, instance *scheduler.AssetInstance) error {
-	s.RLock()
-	defer s.RUnlock()
-	for foundPipelineName, states := range s.State {
-		if foundPipelineName == pipelineName {
-			for key, state := range states {
-				if state.Asset.Name == name {
-					s.State[pipelineName][key] = instance
-					return nil
-				}
-			}
-		}
-	}
-	return nil
-}
-
-func (s *State) SaveState(stateFileDir string) error {
-	// TODO: Implement state saving
-	return nil
-}
-
-func (s *State) CompareState() error {
-	// TODO: Implement state saving
-	return nil
 }
