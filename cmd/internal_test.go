64d36cce9c994176b3c7cd443ea17d58fd8cf689
y-bruin <yuvraj.yadav@getbruin.com>
2024-12-19 15:23:54 +0530

    more changes

--- cmd/internal_test.go
+++ cmd/internal_test.go
@@ -9,15 +9,26 @@ import (
 
 func BenchmarkInternalParsePipeline(b *testing.B) {
 	app := cli.NewApp()
+	parsePipeline := ParsePipeline()
+	parsePipeline.Flags[0] = &cli.BoolFlag{
+		Name:        "column-lineage",
+		Aliases:     []string{"c"},
+		Usage:       "return the column lineage for the given asset",
+		Required:    true,
+		DefaultText: "false",
+	}
 	app.Commands = []*cli.Command{
-		ParsePipeline(),
+		parsePipeline,
 	}
 
+	app.Run([]string{"parse-pipeline"})
 	// Run the benchmark
 	for i := 0; i < b.N; i++ {
-		err := app.RunContext(context.Background(), []string{"internal", "parse-pipeline", "-c", "./testdata/lineage"})
+		err := app.RunContext(context.Background(), []string{
+			"/Users/yuvraj/Workspace/bruin/example/simple-pipeline",
+		})
 		if err != nil {
-			b.Fatalf("Failed to run Lint command: %v", err)
+			b.Fatalf("Failed to run ParsePipeline command: %v", err)
 		}
 	}
 }
