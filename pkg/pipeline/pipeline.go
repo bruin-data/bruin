730bf36ca49e4e907a8a9e7da70aee2f641800e4
Y <yuvraj.yadav@getbruin.com>
2024-12-30 19:15:10 +0530

    Added 'bq.source' asset type (#365)
    * Added 'bq.source' asset type to pipeline.go
    
    * Added support for 'bq.source' asset type in GetConnectionNameForAsset function.

--- pkg/pipeline/pipeline.go
+++ pkg/pipeline/pipeline.go
@@ -41,10 +41,10 @@ const (
 	AssetTypeSynapseQuery         = AssetType("synapse.sql")
 	AssetTypeIngestr              = AssetType("ingestr")
 	AssetTypeTableau              = AssetType("tableau")
-
-	RunConfigFullRefresh = RunConfig("full-refresh")
-	RunConfigStartDate   = RunConfig("start-date")
-	RunConfigEndDate     = RunConfig("end-date")
+	AssetTypeBQSource             = AssetType("bq.source")
+	RunConfigFullRefresh          = RunConfig("full-refresh")
+	RunConfigStartDate            = RunConfig("start-date")
+	RunConfigEndDate              = RunConfig("end-date")
 )
 
 var defaultMapping = map[string]string{
@@ -1025,7 +1025,7 @@ func (p *Pipeline) GetConnectionNameForAsset(asset *Asset) (string, error) {
 	assetType := asset.Type
 	if assetType == AssetTypeIngestr {
 		assetType = IngestrTypeConnectionMapping[asset.Parameters["destination"]]
-	} else if assetType == AssetTypePython || assetType == AssetTypeEmpty {
+	} else if assetType == AssetTypePython || assetType == AssetTypeEmpty || assetType == AssetTypeBQSource {
 		assetType = p.GetMajorityAssetTypesFromSQLAssets(AssetTypeBigqueryQuery)
 	}
 
