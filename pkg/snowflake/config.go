50e06906f0c369dfec65d1702ed3271b6a0ed223
Burak Karakan <burak.karakan@gmail.com>
2025-01-02 10:07:28 +0300

    feat: experimental support for RSA key auth for snowflake

--- pkg/snowflake/config.go
+++ pkg/snowflake/config.go
@@ -1,32 +1,53 @@
 package snowflake
 
 import (
-	"net/url"
-
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"fmt"
 	"github.com/snowflakedb/gosnowflake"
+	"net/url"
 )
 
 type Config struct {
-	Account   string `envconfig:"SNOWFLAKE_ACCOUNT"`
-	Username  string `envconfig:"SNOWFLAKE_USERNAME"`
-	Password  string `envconfig:"SNOWFLAKE_PASSWORD"`
-	Region    string `envconfig:"SNOWFLAKE_REGION"`
-	Role      string `envconfig:"SNOWFLAKE_ROLE"`
-	Database  string `envconfig:"SNOWFLAKE_DATABASE"`
-	Schema    string `envconfig:"SNOWFLAKE_SCHEMA"`
-	Warehouse string `envconfig:"SNOWFLAKE_WAREHOUSE"`
+	Account    string
+	Username   string
+	Password   string
+	Region     string
+	Role       string
+	Database   string
+	Schema     string
+	Warehouse  string
+	PrivateKey string
 }
 
 func (c Config) DSN() (string, error) {
+	authType := gosnowflake.AuthTypeSnowflake
+	if c.PrivateKey != "" {
+		authType = gosnowflake.AuthTypeJwt
+	}
+
 	snowflakeConfig := gosnowflake.Config{
-		Account:   c.Account,
-		User:      c.Username,
-		Password:  c.Password,
-		Region:    c.Region,
-		Role:      c.Role,
-		Database:  c.Database,
-		Schema:    c.Schema,
-		Warehouse: c.Warehouse,
+		Authenticator: authType,
+		Account:       c.Account,
+		User:          c.Username,
+		Password:      c.Password,
+		Region:        c.Region,
+		Role:          c.Role,
+		Database:      c.Database,
+		Schema:        c.Schema,
+		Warehouse:     c.Warehouse,
+		PrivateKey: func() *rsa.PrivateKey {
+			if c.PrivateKey == "" {
+				return nil
+			}
+			key, err := parsePrivateKey(c.PrivateKey)
+			if err != nil {
+				return nil
+			}
+			return key
+		}(),
 	}
 
 	return gosnowflake.DSN(&snowflakeConfig)
@@ -56,4 +77,20 @@ func (c Config) GetIngestrURI() (string, error) {
 
 func (c Config) IsValid() bool {
 	return c.Account != "" && c.Username != "" && c.Password != "" && c.Region != ""
+}
+
+func parsePrivateKey(content string) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode([]byte(content))
+	if block == nil {
+		return nil, errors.New("failed to parse PEM block containing the private key")
+	}
+	privateKey, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	pk, ok := privateKey.(*rsa.PrivateKey)
+	if !ok {
+		return nil, fmt.Errorf("interface convertion. expected type *rsa.PrivateKey, but got %T", privateKey)
+	}
+	return pk, nil
 }
