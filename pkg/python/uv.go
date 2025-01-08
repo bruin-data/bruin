185d355c478cb8aca06caa539130bea1b7465863
Burak Karakan <burak.karakan@gmail.com>
2025-01-08 13:00:17 +0300

    bump ingestr

--- pkg/python/uv.go
+++ pkg/python/uv.go
@@ -35,7 +35,7 @@ var AvailablePythonVersions = map[string]bool{
 const (
 	UvVersion               = "0.5.0"
 	pythonVersionForIngestr = "3.11"
-	ingestrVersion          = "v0.12.4"
+	ingestrVersion          = "v0.12.5"
 )
 
 // UvChecker handles checking and installing the uv package manager.
