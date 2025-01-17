749eedaf756f4641d34a045b7afa730f99b1b27b
y-bruin <yuvraj.yadav@getbruin.com>
2025-01-17 23:00:05 +0530

    added default values in pipeline

--- cmd/render.go
+++ cmd/render.go
@@ -117,7 +117,7 @@ func Render() *cli.Command {
 				return cli.Exit("", 1)
 			}
 
-			asset, err := DefaultPipelineBuilder.CreateAssetFromFile(inputPath)
+			asset, err := DefaultPipelineBuilder.CreateAssetFromFile(inputPath, pl)
 			if err != nil {
 				printError(err, c.String("output"), "Failed to read the asset definition file:")
 				return cli.Exit("", 1)
@@ -177,7 +177,7 @@ func Render() *cli.Command {
 				output:  c.String("output"),
 			}
 
-			return r.Run(inputPath)
+			return r.Run(inputPath, pl)
 		},
 		Before: telemetry.BeforeCommand,
 		After:  telemetry.AfterCommand,
@@ -193,7 +193,7 @@ type queryMaterializer interface {
 }
 
 type taskCreator interface {
-	CreateAssetFromFile(path string) (*pipeline.Asset, error)
+	CreateAssetFromFile(path string, foundPipeline *pipeline.Pipeline) (*pipeline.Asset, error)
 }
 
 type RenderCommand struct {
@@ -205,7 +205,7 @@ type RenderCommand struct {
 	writer io.Writer
 }
 
-func (r *RenderCommand) Run(taskPath string) error {
+func (r *RenderCommand) Run(taskPath string, foundPipeline *pipeline.Pipeline) error {
 	defer RecoverFromPanic()
 
 	if taskPath == "" {
@@ -213,7 +213,7 @@ func (r *RenderCommand) Run(taskPath string) error {
 		return cli.Exit("", 1)
 	}
 
-	task, err := r.builder.CreateAssetFromFile(taskPath)
+	task, err := r.builder.CreateAssetFromFile(taskPath, foundPipeline)
 	if err != nil {
 		r.printErrorOrJsonf("Failed to build asset: %v\n", err.Error())
 		return cli.Exit("", 1)
---
+++ integration-tests/test-pipelines/parse-default-option/assets/asset.py
@@ -0,0 +1,25 @@
+""" @bruin
+
+name: python_asset
+image: python:3.11
+depends:
+    - chess_playground.player_summary
+
+secrets:
+    - key: KEY1
+      inject_as: INJECTED1
+    
+@bruin """
+
+import os
+import duckdb
+
+if os.getenv('INJECTED1') != "value1":
+    raise Exception("KEY1 is not injected correctly")
+
+con = duckdb.connect(database = "duckdb-files/env-run-default-option.db", read_only = False)
+
+con.execute("SELECT * FROM chess_playground.player_summary")
+result = con.fetchall()
+if len(result) != 2:
+    raise Exception("Incorrect number of rows in player_summary")
