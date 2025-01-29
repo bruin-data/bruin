12154b3893d41655fe70150d7c8aa49b10f04f26
Sanju <sanjushahgupta@gmail.com>
2025-01-29 12:05:58 +0100

    upgrade ingestr version

--- pkg/pipeline/pipeline.go
+++ pkg/pipeline/pipeline.go
@@ -499,6 +499,7 @@ var IngestrTypeConnectionMapping = map[string]AssetType{
 	"databricks": AssetTypeDatabricksQuery,
 	"synapse":    AssetTypeSynapseQuery,
 	"duckdb":     AssetTypeDuckDBQuery,
+	"clickhouse": AssetTypeClickHouse,
 }
 
 type SecretMapping struct {
