c15925574b64799c72e69dc5c45eb765bff913d8
Burak Karakan <burak.karakan@gmail.com>
2025-02-25 18:24:20 +0300

    bump uv version

--- pkg/python/uv.go
+++ pkg/python/uv.go
@@ -33,7 +33,7 @@ var AvailablePythonVersions = map[string]bool{
 }
 
 const (
-	UvVersion               = "0.5.0"
+	UvVersion               = "0.6.3"
 	pythonVersionForIngestr = "3.11"
 	ingestrVersion          = "0.13.13"
 )
