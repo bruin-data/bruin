40ffb16ab093a090d4017cf1e77e64586f12ae48
turtleDev <saravjeetamansingh@gmail.com>
2025-03-04 01:11:42 +0530

    git: CurrentCommit(): add support for parsing shared repositories

--- pkg/git/root.go
+++ pkg/git/root.go
@@ -1,10 +1,13 @@
 package git
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"runtime"
 	"strings"
 	"sync"
@@ -121,13 +124,78 @@ func isDirectory(path string) bool {
 	return fileInfo.IsDir()
 }
 
-func CurrentCommit(path string) (string, error) {
+type errDubiousOwnership struct {
+	Path string
+}
+
+func (e errDubiousOwnership) Error() string {
+	return fmt.Sprintf("detected dubious ownership in repository at '%s'", e.Path)
+}
+
+var dubiousOwnershipPattern = regexp.MustCompile("fatal: detected dubious ownership in repository at '(.*)'")
+
+func parseGitError(err error, stderr string) error {
+	matches := dubiousOwnershipPattern.FindStringSubmatch(stderr)
+	if len(matches) == 2 {
+		return errDubiousOwnership{Path: matches[1]}
+	}
+
+	return fmt.Errorf("%w: %s", err, stderr)
+}
+
+func addSafeDirectory(path string) error {
+	cmd := exec.Command(
+		"git",
+		"config",
+		"--global",
+		"--add",
+		"safe.directory",
+		path,
+	)
+	var stderr = new(bytes.Buffer)
+	cmd.Stderr = stderr
+
+	err := cmd.Run()
+	if err != nil {
+		return parseGitError(err, stderr.String())
+	}
+	return nil
+}
+
+func runGitRevParse(path string) (string, error) {
+	var (
+		stdout = new(bytes.Buffer)
+		stderr = new(bytes.Buffer)
+	)
 	command := exec.Command("git", "rev-parse", "HEAD")
 	command.Dir = path
-	res, err := command.Output()
+	command.Stdout = stdout
+	command.Stderr = stderr
+	err := command.Run()
 	if err != nil {
+		return "", parseGitError(err, stderr.String())
+	}
+
+	return strings.TrimSpace(stdout.String()), nil
+}
+
+func CurrentCommit(path string) (string, error) {
+	hash, err := runGitRevParse(path)
+	if err != nil {
+		// git refuses to parse a repository when the calling
+		// user is not the owner of the repository. This can happen
+		// when the repository is cloned from a different user or container.
+		// This can be fixed by adding the repository to the safe.directory
+		var dubiousOwnership errDubiousOwnership
+		if errors.As(err, &dubiousOwnership) {
+			updateErr := addSafeDirectory(dubiousOwnership.Path)
+			if updateErr != nil {
+				return "", fmt.Errorf("failed to add repository to safe.directory: %w", updateErr)
+			}
+			return runGitRevParse(path)
+		}
 		return "", err
 	}
 
-	return strings.TrimSpace(string(res)), nil
+	return hash, nil
 }
