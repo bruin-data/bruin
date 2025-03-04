43b78657e6bc35fe3b861facf7326ba07ba8aa1e
Sanju <sanjushahgupta@gmail.com>
2025-03-04 15:05:07 +0100

    add kinesis connection

--- pkg/config/connections.go
+++ pkg/config/connections.go
@@ -547,3 +547,14 @@ type GCSConnection struct {
 func (c GCSConnection) GetName() string {
 	return c.Name
 }
+
+type KinesisConnection struct {
+	Name                  string `yaml:"name,omitempty" json:"name" mapstructure:"name"`
+	AWS_ACCESS_KEY_ID     string `yaml:"aws_access_key_id,omitempty" json:"aws_access_key_id" mapstructure:"aws_access_key_id"`
+	AWS_SECRET_ACCESS_KEY string `yaml:"aws_secret_access_key,omitempty" json:"aws_secret_access_key" mapstructure:"aws_secret_access_key"`
+	Region                string `yaml:"region,omitempty" json:"region" mapstructure:"region"`
+}
+
+func (c KinesisConnection) GetName() string {
+	return c.Name
+}
