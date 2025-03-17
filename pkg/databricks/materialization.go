c01d5a1534c29aac5b1cff48f233e9ad4b3716f5
Baris Terzioglu <baristerzioglu1432@gmail.com>
2025-03-17 15:05:28 +0100

    add time interval for databricks

--- pkg/databricks/materialization.go
+++ pkg/databricks/materialization.go
@@ -27,6 +27,7 @@ var matMap = AssetMaterializationMap{
 		pipeline.MaterializationStrategyCreateReplace: buildCreateReplaceQuery,
 		pipeline.MaterializationStrategyDeleteInsert:  buildIncrementalQuery,
 		pipeline.MaterializationStrategyMerge:         buildMergeQuery,
+		pipeline.MaterializationStrategyTimeInterval:  buildTimeIntervalQuery,
 	},
 }
 
@@ -130,4 +131,33 @@ func buildCreateReplaceQuery(task *pipeline.Asset, query string) ([]string, erro
 		fmt.Sprintf(`DROP TABLE IF EXISTS %s;`, task.Name),
 		fmt.Sprintf(`ALTER TABLE %s RENAME TO %s;`, tempTableName, task.Name),
 	}, nil
+}
+
+func buildTimeIntervalQuery(asset *pipeline.Asset, query string) ([]string, error) {
+	if asset.Materialization.IncrementalKey == "" {
+		return nil, errors.New("incremental_key is required for time_interval strategy")
+	}
+
+	if asset.Materialization.TimeGranularity == "" {
+		return nil, errors.New("time_granularity is required for time_interval strategy")
+	}
+
+	startVar := "{{start_timestamp}}"
+	endVar := "{{end_timestamp}}"
+	if asset.Materialization.TimeGranularity == pipeline.MaterializationTimeGranularityDate {
+		startVar = "{{start_date}}"
+		endVar = "{{end_date}}"
+	}
+
+	queries := []string{
+		fmt.Sprintf(`DELETE FROM %s WHERE %s BETWEEN '%s' AND '%s'`,
+			asset.Name,
+			asset.Materialization.IncrementalKey,
+			startVar,
+			endVar),
+		fmt.Sprintf(`INSERT INTO %s %s`,
+			asset.Name, query),
+	}
+
+	return queries, nil
 }
