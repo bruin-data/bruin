64e0e1d73cf7276c5bc2703c6d1305eaabf73472
Baris Terzioglu <baristerzioglu1432@gmail.com>
2025-04-03 14:17:55 +0200

    fix:lint

--- pkg/pipeline/pipeline_test.go
+++ pkg/pipeline/pipeline_test.go
@@ -1470,7 +1470,6 @@ func TestIntervalModifiers_ModifyDate(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			time, err := date.ParseTime(tt.baseTime)
