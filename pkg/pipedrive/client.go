043593673b2ba3c90b04d0b1d4eebcbbe9b2e9a9
Sanju <sanjushahgupta@gmail.com>
2025-04-07 14:17:03 +0200

    set up pipedrive client with config struct

--- pkg/pipedrive/client.go
+++ pkg/pipedrive/client.go
@@ -1,1 +1,13 @@
 package pipedrive
+
+type Client struct {
+	config Config
+}
+
+func (c *Client) GetIngestrURI() (string, error) {
+	return c.config.GetIngestrURI(), nil
+}
+
+func NewClient(c Config) *Client {
+	return &Client{config: c}
+}
