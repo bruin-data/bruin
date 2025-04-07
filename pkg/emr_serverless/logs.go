966d83d71e980b380028aaae50802a073bef78e7
turtleDev <saravjeetamansingh@gmail.com>
2025-04-07 17:12:00 +0530

    emr_serverless.spark: refactor log consumer
    This change makes the log consumer easier to read, while also
    making it malleable for extension with additional log types

--- pkg/emr_serverless/logs.go
+++ pkg/emr_serverless/logs.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"compress/gzip"
 	"context"
+	"fmt"
 	"net/url"
 	"strings"
 	"sync"
@@ -13,8 +14,9 @@ import (
 )
 
 type LogLine struct {
-	Stream  string
-	Message string
+	Stream     string
+	Message    string
+	WorkerType string
 }
 
 type logState struct {
@@ -33,32 +35,51 @@ type S3LogConsumer struct {
 	state map[string]logState
 }
 
+type logSource struct {
+	URI    *url.URL
+	Name   string
+	Stream string
+}
+
 func (l *S3LogConsumer) Next() (lines []LogLine) { //nolint
 	l.once.Do(func() {
 		l.state = make(map[string]logState)
 	})
 
-	lines = append(lines, l.readStream("stdout")...)
-	lines = append(lines, l.readStream("stderr")...)
+	for _, source := range l.listLogSources() {
+		lines = append(lines, l.readLogs(source)...)
+	}
 
 	return lines
 }
 
-func (l *S3LogConsumer) readStream(stream string) (lines []LogLine) { //nolint
-
-	if !l.hasNewLogs(stream) {
-		return nil
+func (l *S3LogConsumer) listLogSources() (sources []logSource) {
+	streams := []string{"stdout", "stderr"}
+	for _, stream := range streams {
+		sources = append(sources, logSource{
+			Name:   "SPARK_DRIVER",
+			Stream: stream,
+			URI:    l.driverLogFile(stream),
+		})
 	}
+	return sources
+}
 
+func (l *S3LogConsumer) readLogs(source logSource) []LogLine {
 	logStream, err := l.S3cli.GetObject(l.Ctx, &s3.GetObjectInput{
-		Bucket: &l.URI.Host,
-		Key:    aws.String(l.logFile(stream)),
+		Bucket: &source.URI.Host,
+		Key:    aws.String(strings.TrimPrefix(source.URI.Path, "/")),
 	})
-
 	if err != nil {
 		return nil
 	}
 	defer logStream.Body.Close()
+
+	stateKey := fmt.Sprintf("%s:%s", source.Name, source.Stream)
+	state, exists := l.state[stateKey]
+	if exists && *logStream.ContentLength == state.size {
+		return nil
+	}
 
 	gzReader, err := gzip.NewReader(logStream.Body)
 	if err != nil {
@@ -67,50 +88,34 @@ func (l *S3LogConsumer) readStream(stream string) (lines []LogLine) { //nolint
 	defer gzReader.Close()
 
 	scanner := bufio.NewScanner(gzReader)
-	for range l.state[stream].read {
+	for range l.state[stateKey].read {
 		scanner.Scan()
 	}
 
+	lines := []LogLine{}
 	for scanner.Scan() {
 		lines = append(lines, LogLine{
-			Stream:  stream,
-			Message: strings.TrimSpace(scanner.Text()),
+			WorkerType: source.Name,
+			Stream:     source.Stream,
+			Message:    strings.TrimSpace(scanner.Text()),
 		})
 	}
-	l.state[stream] = logState{
-		read: len(lines) + l.state[stream].read,
+	l.state[stateKey] = logState{
+		read: len(lines) + l.state[stateKey].read,
 		size: *logStream.ContentLength,
 	}
-	return //nolint
+	return lines
 }
 
-func (l *S3LogConsumer) logFile(stream string) string {
-	fullPath := l.URI.JoinPath(
+func (l *S3LogConsumer) driverLogFile(stream string) *url.URL {
+	return l.URI.JoinPath(
 		"applications",
 		l.AppID,
 		"jobs",
 		l.RunID,
 		"SPARK_DRIVER",
 		stream+".gz",
-	).Path
-
-	return strings.TrimPrefix(fullPath, "/")
-}
-
-func (l *S3LogConsumer) hasNewLogs(stream string) bool {
-	state, exists := l.state[stream]
-	if !exists {
-		return true
-	}
-
-	meta, err := l.S3cli.HeadObject(l.Ctx, &s3.HeadObjectInput{
-		Bucket: &l.URI.Host,
-		Key:    aws.String(l.logFile(stream)),
-	})
-	if err != nil {
-		return false
-	}
-	return *meta.ContentLength != state.size
+	)
 }
 
 type NoOpLogConsumer struct{}
