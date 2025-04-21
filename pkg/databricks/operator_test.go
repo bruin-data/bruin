bae44ab87f761409524af64bc5102022c8f610b1
Baris Terzioglu <baristerzioglu1432@gmail.com>
2025-04-21 23:44:36 +0200

    an extractor for each asset databricks

--- pkg/databricks/operator_test.go
+++ pkg/databricks/operator_test.go
@@ -20,6 +20,10 @@ func (m *mockExtractor) ExtractQueriesFromString(content string) ([]*query.Query
 	return res.Get(0).([]*query.Query), res.Error(1)
 }
 
+func (m *mockExtractor) CloneForAsset(ctx context.Context, asset *pipeline.Asset) query.QueryExtractor {
+	return m
+}
+
 func (m *mockExtractor) ReextractQueriesFromSlice(content []string) ([]string, error) {
 	res := m.Called(content)
 	return res.Get(0).([]string), res.Error(1)
