d4e74114f9ba3437226e788bcae318e590f0f877
Baris Terzioglu <baristerzioglu1432@gmail.com>
2025-04-21 23:47:07 +0200

    extractor for each asset athena

--- pkg/athena/operator.go
+++ pkg/athena/operator.go
@@ -27,6 +27,7 @@ type Client interface {
 type queryExtractor interface {
 	ExtractQueriesFromString(content string) ([]*query.Query, error)
 	ReextractQueriesFromSlice(content []string) ([]string, error)
+	CloneForAsset(ctx context.Context, asset *pipeline.Asset) query.QueryExtractor
 }
 
 type connectionFetcher interface {
@@ -53,7 +54,8 @@ func (o BasicOperator) Run(ctx context.Context, ti scheduler.TaskInstance) error
 }
 
 func (o BasicOperator) RunTask(ctx context.Context, p *pipeline.Pipeline, t *pipeline.Asset) error {
-	queries, err := o.extractor.ExtractQueriesFromString(t.ExecutableFile.Content)
+	extractor := o.extractor.CloneForAsset(ctx, t)
+	queries, err := extractor.ExtractQueriesFromString(t.ExecutableFile.Content)
 	if err != nil {
 		return errors.Wrap(err, "cannot extract queries from the task file")
 	}
