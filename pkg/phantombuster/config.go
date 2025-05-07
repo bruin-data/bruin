3f8914600ceaf40eab8f3233a8336824362eb2fc
Sanju <sanjushahgupta@gmail.com>
2025-05-07 16:34:24 +0200

    remove log

--- pkg/phantombuster/config.go
+++ pkg/phantombuster/config.go
@@ -1,13 +1,9 @@
 package phantombuster
 
-import "fmt"
-
 type Config struct {
-	//phantombuster://?api_key=wEVH4Y5nrdS0hvgcMydUGMFbAshiPNP7tmBVHd9f7BE
 	APIKey string `yaml:"api_key" json:"api_key" mapstructure:"api_key"`
 }
 
 func (c *Config) GetIngestrURI() string {
-	fmt.Println("phantombuster://?api_key=" + c.APIKey)
 	return "phantombuster://?api_key=" + c.APIKey
 }
