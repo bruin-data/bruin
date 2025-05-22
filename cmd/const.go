34f4298163e3cffa9a0322562dbf5e6018d6b52e
turtleDev <saravjeetamansingh@gmail.com>
2025-05-22 19:17:15 +0530

    hygiene: refactor variables pipeline mutator

--- cmd/const.go
+++ cmd/const.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"strings"
 
@@ -44,33 +45,13 @@ func variableOverridesMutator(variables []string) pipeline.PipelineMutator {
 	return func(ctx context.Context, p *pipeline.Pipeline) (*pipeline.Pipeline, error) {
 		var overrides = map[string]any{}
 		for _, variable := range variables {
-			var composite map[string]any
-			variable = strings.TrimSpace(variable)
-
-			err := json.Unmarshal([]byte(variable), &composite)
-			if err == nil {
-				for k, v := range composite {
-					overrides[k] = v
-				}
-				continue
-			}
-
-			// this is a heuristic to detect if the variable is a JSON object
-			if strings.HasPrefix(variable, "{") {
+			parsed, err := parseVariable(variable)
+			if err != nil {
 				return nil, fmt.Errorf("invalid variable override %q: %w", variable, err)
 			}
-
-			segments := strings.SplitN(variable, "=", 2)
-			if len(segments) != 2 {
-				return nil, fmt.Errorf("invalid variable override key %q: variable must of form key=value", variable)
+			for key, value := range parsed {
+				overrides[key] = value
 			}
-
-			key := strings.TrimSpace(segments[0])
-			var value any
-			if err := json.Unmarshal([]byte(segments[1]), &value); err != nil {
-				return nil, fmt.Errorf("invalid variable override value %q: %w", variable, err)
-			}
-			overrides[key] = value
 		}
 		err := p.Variables.Merge(overrides)
 		if err != nil {
@@ -78,4 +59,31 @@ func variableOverridesMutator(variables []string) pipeline.PipelineMutator {
 		}
 		return p, nil
 	}
+}
+
+func parseVariable(variable string) (map[string]any, error) {
+	var composite map[string]any
+	variable = strings.TrimSpace(variable)
+
+	err := json.Unmarshal([]byte(variable), &composite)
+	if err == nil {
+		return composite, nil
+	}
+
+	// this is a heuristic to detect if the variable is a JSON object
+	if strings.HasPrefix(variable, "{") {
+		return nil, err
+	}
+
+	segments := strings.SplitN(variable, "=", 2)
+	if len(segments) != 2 {
+		return nil, errors.New("variable must of form key=value")
+	}
+
+	key := strings.TrimSpace(segments[0])
+	var value any
+	if err := json.Unmarshal([]byte(segments[1]), &value); err != nil {
+		return nil, err
+	}
+	return map[string]any{key: value}, nil
 }
