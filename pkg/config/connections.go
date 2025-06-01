c335d95ee4b1699a369b71004ee895dbf2eb281a
Sanju Shah <71315276+sanjushahgupta@users.noreply.github.com>
2025-06-01 17:43:53 +0200

    Spanner: pass service account json instead of base64 credentials
    * allow users to pass service account json content directly instead of base64-encoded credentials
    * change 'spanner' to 'gcp_spanner' in JSON configuration
    * update tests and documentation based on above changes

--- pkg/config/connections.go
+++ pkg/config/connections.go
@@ -746,12 +746,12 @@ func (c ElasticsearchConnection) GetName() string {
 }
 
 type SpannerConnection struct {
-	Name              string `yaml:"name,omitempty" json:"name" mapstructure:"name"`
-	ProjectID         string `yaml:"project_id,omitempty" json:"project_id" mapstructure:"project_id"`
-	InstanceID        string `yaml:"instance_id,omitempty" json:"instance_id" mapstructure:"instance_id"`
-	Database          string `yaml:"database,omitempty" json:"database" mapstructure:"database"`
-	CredentialsBase64 string `yaml:"credentials_base64,omitempty" json:"credentials_base64" mapstructure:"credentials_base64"`
-	CredentialsPath   string `yaml:"credentials_path,omitempty" json:"credentials_path" mapstructure:"credentials_path"`
+	Name               string `yaml:"name,omitempty" json:"name" mapstructure:"name"`
+	ProjectID          string `yaml:"project_id,omitempty" json:"project_id" mapstructure:"project_id"`
+	InstanceID         string `yaml:"instance_id,omitempty" json:"instance_id" mapstructure:"instance_id"`
+	Database           string `yaml:"database,omitempty" json:"database" mapstructure:"database"`
+	ServiceAccountJSON string `yaml:"service_account_json,omitempty" json:"service_account_json,omitempty" mapstructure:"service_account_json"`
+	ServiceAccountFile string `yaml:"service_account_file,omitempty" json:"service_account_file,omitempty" mapstructure:"service_account_file"`
 }
 
 func (c SpannerConnection) GetName() string {
