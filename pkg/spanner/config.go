32f9684f7296ca7f7567dbcc3d2871461fb89542
Sanju <sanjushahgupta@gmail.com>
2025-06-01 16:11:27 +0200

    refactor

--- pkg/spanner/config.go
+++ pkg/spanner/config.go
@@ -6,11 +6,11 @@ import (
 )
 
 type Config struct {
-	ProjectID                  string
-	InstanceID                 string
-	Database                   string
-	ServiceAccountJSON         string
-	ServiceAccountJSONFilePath string
+	ProjectID          string
+	InstanceID         string
+	Database           string
+	ServiceAccountJSON string
+	ServiceAccountFile string
 }
 
 func (c *Config) GetIngestrURI() string {
@@ -18,14 +18,12 @@ func (c *Config) GetIngestrURI() string {
 	q.Set("project_id", c.ProjectID)
 	q.Set("instance_id", c.InstanceID)
 	q.Set("database", c.Database)
+
 	if c.ServiceAccountJSON != "" {
-		creds, err := base64.StdEncoding.DecodeString(c.ServiceAccountJSON)
-		if err != nil {
-			return ""
-		}
-		q.Set("service_account_json", string(creds))
-	} else if c.ServiceAccountJSONFilePath != "" {
-		q.Set("service_account_file_path", c.ServiceAccountJSONFilePath)
+		creds := base64.StdEncoding.EncodeToString([]byte(c.ServiceAccountJSON))
+		q.Set("credentials_base64", creds)
+	} else if c.ServiceAccountFile != "" {
+		q.Set("credentials_path", c.ServiceAccountFile)
 	}
 	return "spanner://?" + q.Encode()
 }
