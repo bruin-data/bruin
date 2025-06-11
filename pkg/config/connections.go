de115c9238cdba7f1b8606e2510f26cf18407fbd
Baris Terzioglu <baristerzioglu1432@gmail.com>
2025-06-11 15:36:46 +0200

    fix: tests

--- pkg/config/connections.go
+++ pkg/config/connections.go
@@ -268,8 +268,8 @@ type SnowflakeConnection struct {
 	Database       string `yaml:"database,omitempty" json:"database,omitempty" mapstructure:"database"`
 	Schema         string `yaml:"schema,omitempty" json:"schema,omitempty" mapstructure:"schema"`
 	Warehouse      string `yaml:"warehouse,omitempty" json:"warehouse,omitempty" mapstructure:"warehouse"`
-	PrivateKeyPath string `yaml:"private_key_path,omitempty" json:"private_key_path,omitempty" jsonschema:"oneof_required=private_key" mapstructure:"private_key_path"`
-	PrivateKey     string `yaml:"private_key,omitempty" json:"private_key,omitempty" jsonschema:"oneof_required=private_key_path" mapstructure:"private_key"`
+	PrivateKeyPath string `yaml:"private_key_path,omitempty" json:"private_key_path,omitempty" jsonschema:"oneof_required=private_key_path" mapstructure:"private_key_path"`
+	PrivateKey     string `yaml:"private_key,omitempty" json:"private_key,omitempty" jsonschema:"oneof_required=private_key" mapstructure:"private_key"`
 }
 
 func (c SnowflakeConnection) MarshalJSON() ([]byte, error) {
