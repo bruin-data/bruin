7fa52cfbd8cbea3c2d365dd8651d7455567a340b
Alberto J. Gomez <alberto.gomez@getbruin.com>
2025-06-19 09:36:52 +0200

    read from env

--- pkg/config/manager.go
+++ pkg/config/manager.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	fs2 "io/fs"
 	"maps"
+	"os"
 	"path/filepath"
 	"reflect"
 	"strings"
@@ -18,6 +19,7 @@ import (
 	"github.com/invopop/jsonschema"
 	errors2 "github.com/pkg/errors"
 	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert/yaml"
 )
 
 type Connections struct {
@@ -240,8 +242,15 @@ func (c *Config) SelectEnvironment(name string) error {
 
 func LoadFromFileOrEnv(fs afero.Fs, path string) (*Config, error) {
 	var config Config
+	var err error
+	envConfig, exists := os.LookupEnv("BRUIN_CONFIG_FILE_CONTENT")
+	if exists {
+		err = yaml.Unmarshal([]byte(envConfig), &config)
+	} else {
+		err = path2.ReadYaml(fs, path, &config)
+	}
 
-	err := path2.ReadYaml(fs, path, &config)
+	
 	if err != nil {
 		return nil, err
 	}
