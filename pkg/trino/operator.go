ba8cff636688a03dcda88973c81128221d1bb6be
Baris Terzioglu <baristerzioglu1432@gmail.com>
2025-07-30 01:07:02 +0200

    execute multi queries|

--- pkg/trino/operator.go
+++ pkg/trino/operator.go
@@ -2,6 +2,7 @@ package trino
 
 import (
 	"context"
+	"strings"
 
 	"github.com/bruin-data/bruin/pkg/config"
 	"github.com/bruin-data/bruin/pkg/pipeline"
@@ -46,10 +47,13 @@ func (o BasicOperator) RunTask(ctx context.Context, p *pipeline.Pipeline, t *pip
 	if !ok {
 		return errors.Errorf("'%s' either does not exist or is not a trino connection", connName)
 	}
-
-	// Execute each query
-	for _, q := range queries {
-		err = conn.RunQueryWithoutResult(ctx, q)
+	splitQueries := strings.Split(strings.TrimSpace(queries[0].Query), ";")
+	for _, q := range splitQueries {
+		if strings.TrimSpace(q) == "" {
+			continue
+		}
+		query := &query.Query{Query: q}
+		err = conn.RunQueryWithoutResult(ctx, query)
 		if err != nil {
 			return err
 		}
