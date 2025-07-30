1a6463fd16b3dd08460391bbabd5054eb848f9dd
jbrand-dsp <jdmbrandis@gmail.com>
2025-07-30 16:19:47 +0200

    added unit tests for scd2-by-col

--- pkg/duckdb/materialization_test.go
+++ pkg/duckdb/materialization_test.go
@@ -342,3 +342,232 @@ func TestBuildDDLQuery(t *testing.T) {
 		})
 	}
 }
+
+func TestBuildSCD2ByColumnQuery(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name        string
+		asset       *pipeline.Asset
+		query       string
+		want        string
+		wantErr     bool
+		fullRefresh bool
+	}{
+		{
+			name: "scd2_no_primary_key",
+			asset: &pipeline.Asset{
+				Name: "my.asset",
+				Materialization: pipeline.Materialization{
+					Type:     pipeline.MaterializationTypeTable,
+					Strategy: pipeline.MaterializationStrategySCD2ByColumn,
+				},
+				Columns: []pipeline.Column{
+					{Name: "id"},
+					{Name: "event_name"},
+					{Name: "ts", Type: "date"},
+				},
+			},
+			query:   "SELECT id, event_name, ts from source_table",
+			wantErr: true,
+		},
+		{
+			name: "scd2_reserved_column_name_is_current",
+			asset: &pipeline.Asset{
+				Name: "my.asset",
+				Materialization: pipeline.Materialization{
+					Type:     pipeline.MaterializationTypeTable,
+					Strategy: pipeline.MaterializationStrategySCD2ByColumn,
+				},
+				Columns: []pipeline.Column{
+					{Name: "id", PrimaryKey: true},
+					{Name: "_is_current"},
+				},
+			},
+			query:   "SELECT id, _is_current from source_table",
+			wantErr: true,
+		},
+		{
+			name: "scd2_reserved_column_name_valid_from",
+			asset: &pipeline.Asset{
+				Name: "my.asset",
+				Materialization: pipeline.Materialization{
+					Type:     pipeline.MaterializationTypeTable,
+					Strategy: pipeline.MaterializationStrategySCD2ByColumn,
+				},
+				Columns: []pipeline.Column{
+					{Name: "id", PrimaryKey: true},
+					{Name: "_valid_from"},
+				},
+			},
+			query:   "SELECT id, _valid_from from source_table",
+			wantErr: true,
+		},
+		{
+			name: "scd2_reserved_column_name_valid_until",
+			asset: &pipeline.Asset{
+				Name: "my.asset",
+				Materialization: pipeline.Materialization{
+					Type:     pipeline.MaterializationTypeTable,
+					Strategy: pipeline.MaterializationStrategySCD2ByColumn,
+				},
+				Columns: []pipeline.Column{
+					{Name: "id", PrimaryKey: true},
+					{Name: "_valid_until"},
+				},
+			},
+			query:   "SELECT id, _valid_until from source_table",
+			wantErr: true,
+		},
+		{
+			name: "scd2_basic_column_change_detection and partitioning",
+			asset: &pipeline.Asset{
+				Name: "my.asset",
+				Materialization: pipeline.Materialization{
+					Type:        pipeline.MaterializationTypeTable,
+					Strategy:    pipeline.MaterializationStrategySCD2ByColumn,
+					PartitionBy: "id",
+				},
+				Columns: []pipeline.Column{
+					{Name: "id", PrimaryKey: true},
+					{Name: "col1"},
+					{Name: "col2"},
+				},
+			},
+			query: "SELECT id, col1, col2 from source_table",
+			want: 
+				"CREATE OR REPLACE TABLE my.asset AS\n" +
+					"WITH\n" +
+					"time_now AS (\n" +
+					"\tSELECT CURRENT_TIMESTAMP AS now\n" +
+					"),\n" +
+					"source AS (\n" +
+					"\tSELECT id, col1, col2,\n" +
+					"\tTRUE as _matched_by_source\n" +
+					"\tFROM (SELECT id, col1, col2 from source_table\n" +
+					"\t)\n" +
+					"),\n" +
+					"target AS (\n" +
+					"\tSELECT id, col1, col2, _valid_from, _valid_until, _is_current,\n" +
+					"\tTRUE as _matched_by_target FROM my.asset\n" +
+					"),\n" +
+					"current_data AS (\n" +
+					"\tSELECT id, col1, col2, _valid_from, _valid_until, _is_current, _matched_by_target\n" +
+					"\tFROM target as t\n" +
+					"\tWHERE _is_current = TRUE\n" +
+					"),\n" +
+					"--current or updated (expired) existing rows from target\n" +
+					"to_keep AS (\n" +
+					"\tSELECT t.id, t.col1, t.col2,\n" +
+					"\tt._valid_from,\n" +
+					"\t\tCASE\n" +
+					"\t\t\tWHEN _matched_by_source IS NOT NULL AND (t.col1 != s.col1 OR t.col2 != s.col2) THEN (SELECT now FROM time_now)\n" +
+					"\t\t\tWHEN _matched_by_source IS NULL THEN (SELECT now FROM time_now)\n" +
+					"\t\t\tELSE t._valid_until\n" +
+					"\t\tEND AS _valid_until,\n" +
+					"\t\tCASE\n" +
+					"\t\t\tWHEN _matched_by_source IS NOT NULL AND (t.col1 != s.col1 OR t.col2 != s.col2) THEN FALSE\n" +
+					"\t\t\tWHEN _matched_by_source IS NULL THEN FALSE\n" +
+					"\t\t\tELSE t._is_current\n" +
+					"\t\tEND AS _is_current\n" +
+					"\tFROM target t\n" +
+					"\tLEFT JOIN source s ON (t.id = s.id) AND t._is_current = TRUE\n" +
+					"),\n" +
+					"--new/updated rows from source\n" +
+					"to_insert AS (\n" +
+					"\tSELECT s.id AS id, s.col1 AS col1, s.col2 AS col2,\n" +
+					"\t(SELECT now FROM time_now) AS _valid_from,\n" +
+					"\tTIMESTAMP '9999-12-31 23:59:59' AS _valid_until,\n" +
+					"\tTRUE AS _is_current\n" +
+					"\tFROM source s\n" +
+					"\tLEFT JOIN current_data t ON (t.id = s.id)\n" +
+					"\tWHERE (_matched_by_target IS NULL) OR (t.col1 != s.col1 OR t.col2 != s.col2)\n" +
+					")\n" +
+					"SELECT id, col1, col2, _valid_from, _valid_until, _is_current FROM to_keep\n" +
+					"UNION ALL\n" +
+					"SELECT id, col1, col2, _valid_from, _valid_until, _is_current FROM to_insert;",
+		},
+		{
+			name: "scd2_multiple_primary_keys",
+			asset: &pipeline.Asset{
+				Name: "my.asset",
+				Materialization: pipeline.Materialization{
+					Type:     pipeline.MaterializationTypeTable,
+					Strategy: pipeline.MaterializationStrategySCD2ByColumn,
+				},
+				Columns: []pipeline.Column{
+					{Name: "id", PrimaryKey: true},
+					{Name: "category", PrimaryKey: true},
+					{Name: "name"},
+				},
+			},
+			query: "SELECT id, category, name from source_table",
+			want: 
+				"CREATE OR REPLACE TABLE my.asset AS\n" +
+					"WITH\n" +
+					"time_now AS (\n" +
+					"\tSELECT CURRENT_TIMESTAMP AS now\n" +
+					"),\n" +
+					"source AS (\n" +
+					"\tSELECT id, category, name,\n" +
+					"\tTRUE as _matched_by_source\n" +
+					"\tFROM (SELECT id, category, name from source_table\n" +
+					"\t)\n" +
+					"),\n" +
+					"target AS (\n" +
+					"\tSELECT id, category, name, _valid_from, _valid_until, _is_current,\n" +
+					"\tTRUE as _matched_by_target FROM my.asset\n" +
+					"),\n" +
+					"current_data AS (\n" +
+					"\tSELECT id, category, name, _valid_from, _valid_until, _is_current, _matched_by_target\n" +
+					"\tFROM target as t\n" +
+					"\tWHERE _is_current = TRUE\n" +
+					"),\n" +
+					"--current or updated (expired) existing rows from target\n" +
+					"to_keep AS (\n" +
+					"\tSELECT t.id, t.category, t.name,\n" +
+					"\tt._valid_from,\n" +
+					"\t\tCASE\n" +
+					"\t\t\tWHEN _matched_by_source IS NOT NULL AND (t.name != s.name) THEN (SELECT now FROM time_now)\n" +
+					"\t\t\tWHEN _matched_by_source IS NULL THEN (SELECT now FROM time_now)\n" +
+					"\t\t\tELSE t._valid_until\n" +
+					"\t\tEND AS _valid_until,\n" +
+					"\t\tCASE\n" +
+					"\t\t\tWHEN _matched_by_source IS NOT NULL AND (t.name != s.name) THEN FALSE\n" +
+					"\t\t\tWHEN _matched_by_source IS NULL THEN FALSE\n" +
+					"\t\t\tELSE t._is_current\n" +
+					"\t\tEND AS _is_current\n" +
+					"\tFROM target t\n" +
+					"\tLEFT JOIN source s ON (t.id = s.id AND t.category = s.category) AND t._is_current = TRUE\n" +
+					"),\n" +
+					"--new/updated rows from source\n" +
+					"to_insert AS (\n" +
+					"\tSELECT s.id AS id, s.category AS category, s.name AS name,\n" +
+					"\t(SELECT now FROM time_now) AS _valid_from,\n" +
+					"\tTIMESTAMP '9999-12-31 23:59:59' AS _valid_until,\n" +
+					"\tTRUE AS _is_current\n" +
+					"\tFROM source s\n" +
+					"\tLEFT JOIN current_data t ON (t.id = s.id AND t.category = s.category)\n" +
+					"\tWHERE (_matched_by_target IS NULL) OR (t.name != s.name)\n" +
+					")\n" +
+					"SELECT id, category, name, _valid_from, _valid_until, _is_current FROM to_keep\n" +
+					"UNION ALL\n" +
+					"SELECT id, category, name, _valid_from, _valid_until, _is_current FROM to_insert;",
+					
+			
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			m := NewMaterializer(tt.fullRefresh)
+			render, err := m.Render(tt.asset, tt.query)
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				assert.Equal(t, tt.want, render)
+			}
+		})
+	}
+}
