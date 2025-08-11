b3cf019895bd544050554d3f587755d6131173f8
jbrand-dsp <jdmbrandis@gmail.com>
2025-08-11 21:30:15 +0200

    streamlined tests

--- integration-tests/cloud-integration-tests/bigquery/bigquery_test.go
+++ integration-tests/cloud-integration-tests/bigquery/bigquery_test.go
@@ -172,18 +172,6 @@ func TestBigQueryWorkflows(t *testing.T) {
 					Name: "bigquery-merge-with-nulls",
 					Steps: []e2e.Task{
 						{
-							Name:    "restore the initial nulltable table",
-							Command: "cp",
-							Args:    []string{filepath.Join(tempDir, "test-pipelines/nullable-pipeline/resources/nulltable_initial.sql"), filepath.Join(tempDir, "test-pipelines/nullable-pipeline/assets/nulltable.sql")},
-							Env:     []string{},
-							Expected: e2e.Output{
-								ExitCode: 0,
-							},
-							Asserts: []func(*e2e.Task) error{
-								e2e.AssertByExitCode,
-							},
-						},
-						{
 							Name:    "create the initial table",
 							Command: binary,
 							Args:    append(append([]string{"run"}, configFlags...), "--full-refresh", filepath.Join(tempDir, "test-pipelines/nullable-pipeline/assets/nulltable.sql")),
