22bc31e6f54cea13d5432cc7e841058b3056fa8a
jbrand-dsp <jdmbrandis@gmail.com>
2025-08-12 17:54:13 +0200

    added temp dir scd2-by-col

--- integration-tests/cloud-integration-tests/postgres/postgres_test.go
+++ integration-tests/cloud-integration-tests/postgres/postgres_test.go
@@ -23,6 +23,8 @@ func TestPostgresWorkflows(t *testing.T) {
 
 	configFlags := []string{"--config-file", filepath.Join(projectRoot, "integration-tests/cloud-integration-tests/.bruin.cloud.yml")}
 
+	tempDir := t.TempDir()
+
 	tests := []struct {
 		name     string
 		workflow e2e.Workflow
@@ -67,10 +69,9 @@ func TestPostgresWorkflows(t *testing.T) {
 				Name: "scd2_by_column",
 				Steps: []e2e.Task{
 					{
-						Name:    "scd2-by-column: restore menu asset to initial state",
-						Command: "cp",
-						Args:    []string{filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/resources/menu_original.sql"), filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/scd2-by-column-pipeline/assets/menu.sql")},
-						Env:     []string{},
+						Name:    "scd2-by-column: create test directory",
+						Command: "mkdir",
+						Args:    []string{"-p", filepath.Join(tempDir, "test-scd2-by-column")},
 						Expected: e2e.Output{
 							ExitCode: 0,
 						},
@@ -79,9 +80,45 @@ func TestPostgresWorkflows(t *testing.T) {
 						},
 					},
 					{
+						Name:    "scd2-by-column: initialize git repository",
+						Command: "git",
+						Args:    []string{"init"},
+						WorkingDir: filepath.Join(tempDir, "test-scd2-by-column"),
+						Expected: e2e.Output{
+							ExitCode: 0,
+						},
+						Asserts: []func(*e2e.Task) error{
+							e2e.AssertByExitCode,
+						},
+					},
+					{
+						Name:       "scd2-by-column: copy pipeline files",
+						Command:    "cp",
+						Args:       []string{"-a", filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/scd2-by-column-pipeline"), filepath.Join(tempDir, "test-scd2-by-column")},
+						WorkingDir: filepath.Join(tempDir, "test-scd2-by-column"),
+						Expected: e2e.Output{
+							ExitCode: 0,
+						},
+						Asserts: []func(*e2e.Task) error{
+							e2e.AssertByExitCode,
+						},
+					},
+					// {
+					// 	Name:    "scd2-by-column: restore menu asset to initial state",
+					// 	Command: "cp",
+					// 	Args:    []string{filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/resources/menu_original.sql"), filepath.Join(tempDir, "test-scd2-by-column/scd2-by-column-pipeline/assets/menu.sql")},
+					// 	Env:     []string{},
+					// 	Expected: e2e.Output{
+					// 		ExitCode: 0,
+					// 	},
+					// 	Asserts: []func(*e2e.Task) error{
+					// 		e2e.AssertByExitCode,
+					// 	},
+					// },
+					{
 						Name:    "scd2-by-column: create the initial table",
 						Command: binary,
-						Args:    append(append([]string{"run"}, configFlags...), "--full-refresh", "--env", "default", filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/scd2-by-column-pipeline")),
+						Args:    append(append([]string{"run"}, configFlags...), "--full-refresh", "--env", "default", filepath.Join(tempDir, "test-scd2-by-column/scd2-by-column-pipeline/assets/menu.sql")),
 						Env:     []string{},
 						Expected: e2e.Output{
 							ExitCode: 0,
@@ -93,11 +130,11 @@ func TestPostgresWorkflows(t *testing.T) {
 					{
 						Name:    "scd2-by-column: query the initial table",
 						Command: binary,
-						Args:    append(append([]string{"query"}, configFlags...), "--env", "default", "--asset", filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/scd2-by-column-pipeline/assets/menu.sql"), "--query", "SELECT ID, Name, Price, _is_current FROM test.menu ORDER BY ID, _valid_from;", "--output", "csv"),
+						Args:    append(append([]string{"query"}, configFlags...), "--connection", "postgres-default", "--query", "SELECT ID, Name, Price, _is_current FROM test.menu ORDER BY ID, _valid_from;", "--output", "csv"),
 						Env:     []string{},
 						Expected: e2e.Output{
 							ExitCode: 0,
-							CSVFile:  filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/scd2-by-column-pipeline/expectations/scd2_by_col_expected_initial.csv"),
+							CSVFile:  filepath.Join(tempDir, "test-scd2-by-column/scd2-by-column-pipeline/expectations/scd2_by_col_expected_initial.csv"),
 						},
 						Asserts: []func(*e2e.Task) error{
 							e2e.AssertByExitCode,
@@ -107,7 +144,7 @@ func TestPostgresWorkflows(t *testing.T) {
 					{
 						Name:    "scd2-by-column: copy menu_updated_01.sql to menu.sql",
 						Command: "cp",
-						Args:    []string{filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/resources/menu_updated_01.sql"), filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/scd2-by-column-pipeline/assets/menu.sql")},
+						Args:    []string{filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/resources/menu_updated_01.sql"), filepath.Join(tempDir, "test-scd2-by-column/scd2-by-column-pipeline/assets/menu.sql")},
 						Env:     []string{},
 						Expected: e2e.Output{
 							ExitCode: 0,
@@ -119,7 +156,7 @@ func TestPostgresWorkflows(t *testing.T) {
 					{
 						Name:    "scd2-by-column: run menu_updated_01.sql with SCD2 materialization",
 						Command: binary,
-						Args:    append(append([]string{"run"}, configFlags...), "--env", "default", filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/scd2-by-column-pipeline/assets/menu.sql")),
+						Args:    append(append([]string{"run"}, configFlags...), "--env", "default", filepath.Join(tempDir, "test-scd2-by-column/scd2-by-column-pipeline/assets/menu.sql")),
 						Env:     []string{},
 						Expected: e2e.Output{
 							ExitCode: 0,
@@ -131,11 +168,11 @@ func TestPostgresWorkflows(t *testing.T) {
 					{
 						Name:    "scd2-by-column: query the updated table 01",
 						Command: binary,
-						Args:    append(append([]string{"query"}, configFlags...), "--env", "default", "--asset", filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/scd2-by-column-pipeline/assets/menu.sql"), "--query", "SELECT ID, Name, Price, _is_current FROM test.menu ORDER BY ID, _valid_from;", "--output", "csv"),
+						Args:    append(append([]string{"query"}, configFlags...), "--connection", "postgres-default", "--query", "SELECT ID, Name, Price, _is_current FROM test.menu ORDER BY ID, _valid_from;", "--output", "csv"),
 						Env:     []string{},
 						Expected: e2e.Output{
 							ExitCode: 0,
-							CSVFile:  filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/scd2-by-column-pipeline/expectations/scd2_by_col_expected_updated_01.csv"),
+							CSVFile:  filepath.Join(tempDir, "test-scd2-by-column/scd2-by-column-pipeline/expectations/scd2_by_col_expected_updated_01.csv"),
 						},
 						Asserts: []func(*e2e.Task) error{
 							e2e.AssertByExitCode,
@@ -145,7 +182,7 @@ func TestPostgresWorkflows(t *testing.T) {
 					{
 						Name:    "scd2-by-column: copy menu_updated_02.sql to menu.sql",
 						Command: "cp",
-						Args:    []string{filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/resources/menu_updated_02.sql"), filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/scd2-by-column-pipeline/assets/menu.sql")},
+						Args:    []string{filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/resources/menu_updated_02.sql"), filepath.Join(tempDir, "test-scd2-by-column/scd2-by-column-pipeline/assets/menu.sql")},
 						Env:     []string{},
 						Expected: e2e.Output{
 							ExitCode: 0,
@@ -157,7 +194,7 @@ func TestPostgresWorkflows(t *testing.T) {
 					{
 						Name:    "scd2-by-column: run menu_updated_02.sql with SCD2 materialization",
 						Command: binary,
-						Args:    append(append([]string{"run"}, configFlags...), "--env", "default", filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/scd2-by-column-pipeline/assets/menu.sql")),
+						Args:    append(append([]string{"run"}, configFlags...), "--env", "default", filepath.Join(tempDir, "test-scd2-by-column/scd2-by-column-pipeline/assets/menu.sql")),
 						Env:     []string{},
 						Expected: e2e.Output{
 							ExitCode: 0,
@@ -169,11 +206,11 @@ func TestPostgresWorkflows(t *testing.T) {
 					{
 						Name:    "scd2-by-column: query the updated table 02",
 						Command: binary,
-						Args:    append(append([]string{"query"}, configFlags...), "--env", "default", "--asset", filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/scd2-by-column-pipeline/assets/menu.sql"), "--query", "SELECT ID, Name, Price, _is_current FROM test.menu ORDER BY ID, _valid_from;", "--output", "csv"),
+						Args:    append(append([]string{"query"}, configFlags...), "--connection", "postgres-default", "--query", "SELECT ID, Name, Price, _is_current FROM test.menu ORDER BY ID, _valid_from;", "--output", "csv"),
 						Env:     []string{},
 						Expected: e2e.Output{
 							ExitCode: 0,
-							CSVFile:  filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/scd2-by-column-pipeline/expectations/scd2_by_col_expected_updated_02.csv"),
+							CSVFile:  filepath.Join(tempDir, "test-scd2-by-column/scd2-by-column-pipeline/expectations/scd2_by_col_expected_updated_02.csv"),
 						},
 						Asserts: []func(*e2e.Task) error{
 							e2e.AssertByExitCode,
@@ -183,7 +220,7 @@ func TestPostgresWorkflows(t *testing.T) {
 					{
 						Name:    "scd2-by-column: drop the table (expect error but table will be dropped)",
 						Command: binary,
-						Args:    []string{"query", "--config-file", filepath.Join(currentFolder, "../.bruin.cloud.yml"), "--asset", filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/scd2-by-column-pipeline/assets/menu.sql"), "--query", "DROP TABLE IF EXISTS test.menu;"},
+						Args:    []string{"query", "--config-file", filepath.Join(currentFolder, "../.bruin.cloud.yml"), "--query", "DROP TABLE IF EXISTS test.menu;"},
 						Env:     []string{},
 						Expected: e2e.Output{
 							ExitCode: 1, // Expect failure due to "field descriptions not available for DDL statements" - specific to PostgresSQL driver
@@ -195,7 +232,7 @@ func TestPostgresWorkflows(t *testing.T) {
 					{
 						Name:    "scd2-by-column: confirm the table is dropped",
 						Command: binary,
-						Args:    []string{"query", "--config-file", filepath.Join(currentFolder, "../.bruin.cloud.yml"), "--asset", filepath.Join(currentFolder, "test-pipelines/scd2-pipelines/scd2-by-column-pipeline/assets/menu.sql"), "--query", "SELECT * FROM test.menu;"},
+						Args:    []string{"query", "--config-file", filepath.Join(currentFolder, "../.bruin.cloud.yml"), "--query", "SELECT * FROM test.menu;"},
 						Env:     []string{},
 						Expected: e2e.Output{
 							ExitCode: 1,
