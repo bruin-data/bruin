2ea2b2e7a2b4896b625acf298f467c342582f4ea
jbrand-dsp <jdmbrandis@gmail.com>
2025-08-14 19:57:37 +0200

    added sf table sensor testpipe

---
+++ integration-tests/cloud-integration-tests/snowflake/test-pipelines/table-sensor-pipeline/assets/create_table.sql
@@ -0,0 +1,23 @@
+/* @bruin
+name: dataset.datatable
+type: sf.sql
+
+materialization:
+  type: table
+
+columns:
+  - name: ID
+    type: INTEGER
+    description: "Unique identifier for Employee"
+    primary_key: true
+  - name: Name
+    type: VARCHAR
+    description: "Name of the Employee"
+    primary_key: true
+@bruin */
+
+SELECT 1 AS ID, 'Colin' AS Name
+UNION ALL
+SELECT 2 AS ID, 'John' AS Name
+UNION ALL
+SELECT 4 AS ID, 'Jane' AS Name
