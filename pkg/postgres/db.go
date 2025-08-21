7b7e1825d4e4407153001c117ad7a0946b634cd3
jbrand-dsp <jdmbrandis@gmail.com>
2025-08-21 15:00:22 +0200

    corrected table formatting

--- pkg/postgres/db.go
+++ pkg/postgres/db.go
@@ -776,22 +776,27 @@ func (c *Client) BuildTableExistsQuery(tableName string) (string, error) {
 	tableComponents := strings.Split(tableName, ".")
 	for _, component := range tableComponents {
 		if component == "" {
-			return "", fmt.Errorf("table name must be in format schema.table, '%s' given", tableName)
+			return "", fmt.Errorf("table name must be in format schema.table or table, '%s' given", tableName)
 		}
 	}
-
-	if len(tableComponents) != 2 {
-		return "", fmt.Errorf("table name must be in format schema.table, '%s' given", tableName)
+	
+	var schemaName string
+	switch len(tableComponents) {
+	case 1:
+		schemaName = "public"
+		tableName = tableComponents[0]
+	case 2:
+		schemaName = tableComponents[0]
+		tableName = tableComponents[1]
+	default:
+		return "", fmt.Errorf("table name must be in format schema.table or table, '%s' given", tableName)
 	}
-
-	schemaName := strings.ToLower(tableComponents[0])
-	targetTable := strings.ToLower(tableComponents[1])
+	targetTable := tableName
 
 	query := fmt.Sprintf(
 		"SELECT COUNT(*) FROM pg_catalog.pg_tables WHERE schemaname = '%s' AND tablename = '%s'",
 		schemaName,
 		targetTable,
 	)
-
 	return strings.TrimSpace(query), nil
 }
