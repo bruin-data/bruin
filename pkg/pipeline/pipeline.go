b2f88625afd31a49319b02c71799670c30dacce2
Mustafa <mstfa.ersan@hotmail.com>
2025-09-03 13:42:07 +0300

    add start_date to asset structure

--- pkg/pipeline/pipeline.go
+++ pkg/pipeline/pipeline.go
@@ -721,6 +721,7 @@ type Asset struct { //nolint:recvcheck
 	Image             string             `json:"image" yaml:"image,omitempty" mapstructure:"image"`
 	Instance          string             `json:"instance" yaml:"instance,omitempty" mapstructure:"instance"`
 	Owner             string             `json:"owner" yaml:"owner,omitempty" mapstructure:"owner"`
+	StartDate         string             `json:"start_date" yaml:"start_date,omitempty" mapstructure:"start_date"`
 	Tier              int                `json:"tier,omitempty" yaml:"tier,omitempty" mapstructure:"tier"`
 	ExecutableFile    ExecutableFile     `json:"executable_file" yaml:"-" mapstructure:"-"`
 	DefinitionFile    TaskDefinitionFile `json:"definition_file" yaml:"-" mapstructure:"-"`
@@ -1166,7 +1167,7 @@ func (a *Asset) FormatContent() ([]byte, error) {
 
 	yamlConfig := buf.Bytes()
 
-	keysToAddSpace := []string{"custom_checks", "depends", "columns", "materialization", "secrets", "parameters"}
+	keysToAddSpace := []string{"custom_checks", "depends", "columns", "materialization", "secrets", "parameters", "start_date"}
 	for _, key := range keysToAddSpace {
 		yamlConfig = bytes.ReplaceAll(yamlConfig, []byte("\n"+key+":"), []byte("\n\n"+key+":"))
 	}
