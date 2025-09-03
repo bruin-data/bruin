1e1d4080e9e505f7da7a623df110ca8ddeddc67e
Alberto J. Gomez <alberto.gomez@getbruin.com>
2025-09-03 18:11:08 +0200

    remove extras

--- cmd/run.go
+++ cmd/run.go
@@ -1687,3 +1687,21 @@ func ApplyAllFilters(ctx context.Context, f *Filter, s *scheduler.Scheduler, p *
 	}
 	return nil
 }
+
+type assetCounter interface {
+	GetAssetCountWithTasksPending() int
+}
+
+type lintChecker func(ctx context.Context, foundPipeline *pipeline.Pipeline, pipelinePath string, logger logger.Logger, validateOnlyAssetLevel bool) error
+
+func Validate(shouldvalidate bool, assetCounter assetCounter, lintChecker lintChecker, ctx context.Context, foundPipeline *pipeline.Pipeline, pipelinePath string, logger logger.Logger) error {
+	if !shouldvalidate {
+		return nil
+	}
+
+	if assetCounter.GetAssetCountWithTasksPending() > 0 {
+		return lintChecker(ctx, foundPipeline, pipelinePath, logger, false)
+	}
+
+	return lintChecker(ctx, foundPipeline, pipelinePath, logger, true)
+}
