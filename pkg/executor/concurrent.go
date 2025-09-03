96c542e5082aa0fbf73442c6b1f5425e35b3ea40
Burak Karakan <burak.karakan@gmail.com>
2025-09-03 16:11:26 +0300

    remove date from log timestamp

--- pkg/executor/concurrent.go
+++ pkg/executor/concurrent.go
@@ -38,7 +38,8 @@ const (
 	KeyVerbose contextKey = iota
 	ContextLogger
 
-	timeFormat = "2006-01-02 15:04:05"
+	// Log timestamps should include only the time (no date)
+	timeFormat = "15:04:05"
 )
 
 type FormattingOptions struct {
