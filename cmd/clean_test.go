8fea3ea6ed56098990d14be43339d060440634c9
jbrand-dsp <jdmbrandis@gmail.com>
2025-09-16 21:09:11 +0200

    chore: linting

--- cmd/clean_test.go
+++ cmd/clean_test.go
@@ -262,6 +262,7 @@ func TestCleanCommand_confirmUvCacheClean(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			output := []string{}
 			cmd := &CleanCommand{
 				errorPrinter: &mockOutputPrinter{output: &output},
@@ -274,6 +275,7 @@ func TestCleanCommand_confirmUvCacheClean(t *testing.T) {
 		})
 	}
 }
+
 func TestCleanCommand_confirmUvCacheClean_Error(t *testing.T) {
 	t.Parallel()
 
