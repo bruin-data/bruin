5eeb71d5a485a22311f09faeb8901360f9246f61
jbrand-dsp <jdmbrandis@gmail.com>
2025-09-18 11:50:30 +0200

    added integration tests

---
+++ integration-tests/test-pipelines/float-test-pipeline/assets/float_test_asset.sql
@@ -0,0 +1,8 @@
+-- Test asset for float value formatting
+SELECT 
+    1.5 as simple_float,
+    2.0 as another_float,
+    3.14159 as pi,
+    0.1 as tenth,
+    123.456 as complex_float,
+    0.001 as small_float
