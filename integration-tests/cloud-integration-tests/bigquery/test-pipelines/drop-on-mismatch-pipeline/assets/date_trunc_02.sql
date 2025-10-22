7b8d251f81bb6b1a6f62bcd774b7a7ee83111c7c
jbrand-dsp <jdmbrandis@gmail.com>
2025-10-22 12:46:12 +0200

    added integration test

---
+++ integration-tests/cloud-integration-tests/bigquery/test-pipelines/drop-on-mismatch-pipeline/assets/date_trunc_02.sql
@@ -0,0 +1,18 @@
+/* @bruin
+name: bq_test.date_trunc_02
+type: bq.sql
+materialization:
+  type: table
+  strategy: create+replace
+  partition_by: DATETIME_TRUNC(date_timestamp, MONTH)
+
+columns:
+  - name: date_timestamp
+    type: DATETIME
+  - name: created_at
+    type: DATE
+
+@bruin */
+
+SELECT CAST(current_timestamp() as DATETIME) as date_timestamp,
+DATE(TIMESTAMP_SECONDS({{start_date}})) as created_at
---
+++ integration-tests/cloud-integration-tests/bigquery/test-pipelines/drop-on-mismatch-pipeline/resources/ingest_data.sql
@@ -0,0 +1,35 @@
+/* @bruin
+
+name: ddl_full_refresh.ddl
+type: bq.sql
+
+materialization:
+  type: table
+  strategy: append
+  partition_by: TIMESTAMP_TRUNC(logged_at, DAY)
+  cluster_by: 
+    - channel, site_name
+
+columns:
+  - name: logged_at
+    type: TIMESTAMP
+    description: "Timestamp of the log"
+  - name: company
+    type: STRING
+    description: "Company name"
+  - name: site_name
+    type: STRING
+    description: "Site name"
+  - name: channel
+    type: STRING 
+    description: "Channel name"
+
+@bruin */
+
+SELECT TIMESTAMP '2024-01-01 00:00:05' AS logged_at, 'Company A' AS company, 'Site A' AS site_name, 'Channel A' AS channel
+UNION ALL
+SELECT TIMESTAMP '2024-01-01 00:00:05' AS logged_at, 'Company B' AS company, 'Site B' AS site_name, 'Channel B' AS channel
+UNION ALL
+SELECT TIMESTAMP '2024-01-01 00:00:05' AS logged_at, 'Company C' AS company, 'Site C' AS site_name, 'Channel C' AS channel
+UNION ALL
+SELECT TIMESTAMP '2024-01-01 00:00:05' AS logged_at, 'Company D' AS company, 'Site D' AS site_name, 'Channel D' AS channel
