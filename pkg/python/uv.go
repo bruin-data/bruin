e5f466149b9be714388d7f7ec8a86e658f6b5b6f
karakanb <16530606+karakanb@users.noreply.github.com>
2025-11-11 15:02:46 +0000

    chore: bump ingestr to v0.14.101

--- pkg/python/uv.go
+++ pkg/python/uv.go
@@ -39,7 +39,7 @@ var AvailablePythonVersions = map[string]bool{
 const (
 	UvVersion               = "0.6.16"
 	pythonVersionForIngestr = "3.11"
-	ingestrVersion          = "0.14.100"
+	ingestrVersion          = "0.14.101"
 	sqlfluffVersion         = "3.4.1"
 )
 
