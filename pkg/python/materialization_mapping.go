e7cb6cc2f7377bdf570458638f22cb00815afa1a
Mustafa Ersan <mstfa.ersan@hotmail.com>
2025-11-17 11:27:05 +0300

    create meterilazation mapping

---
+++ pkg/python/materialization_mapping.go
@@ -0,0 +1,29 @@
+package python
+
+import "github.com/bruin-data/bruin/pkg/pipeline"
+
+var BruinToIngestrStrategyMap = map[pipeline.MaterializationStrategy]string{
+	pipeline.MaterializationStrategyCreateReplace:  "replace",
+	pipeline.MaterializationStrategyAppend:         "append",
+	pipeline.MaterializationStrategyMerge:          "merge",
+	pipeline.MaterializationStrategyDeleteInsert:   "delete+insert",
+}
+
+
+var SupportedPythonMaterializationStrategies = []pipeline.MaterializationStrategy{
+	pipeline.MaterializationStrategyCreateReplace,
+	pipeline.MaterializationStrategyAppend,
+	pipeline.MaterializationStrategyMerge,
+	pipeline.MaterializationStrategyDeleteInsert,
+	pipeline.MaterializationStrategyTruncateInsert,
+}
+
+func IsPythonMaterializationStrategySupported(strategy pipeline.MaterializationStrategy) bool {
+	_, exists := BruinToIngestrStrategyMap[strategy]
+	return exists
+}
+
+func TranslateBruinStrategyToIngestr(strategy pipeline.MaterializationStrategy) (string, bool) {
+	ingestrStrategy, exists := BruinToIngestrStrategyMap[strategy]
+	return ingestrStrategy, exists
+}
