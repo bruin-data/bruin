1df4cc2e55d4cde09c316c42e2e776934e07cea5
jbrand-dsp <jdmbrandis@gmail.com>
2025-12-09 13:57:28 +0100

    corrected check

--- templates/duckdb-example/assets/product_categories.sql
+++ templates/duckdb-example/assets/product_categories.sql
@@ -11,7 +11,7 @@ columns:
     primary_key: true
     checks:
       - name: not_null
-      - name: negative
+      - name: non_negative
   - name: category_name
     type: VARCHAR
     description: "Name of the product category"
