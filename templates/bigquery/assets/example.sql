e62eeddf5df31f6508f1b9d33662b8f956e54409
Burak Karakan <burak.karakan@gmail.com>
2026-01-09 18:29:52 +0300

    create a bigquery template

---
+++ templates/bigquery/assets/example.sql
@@ -0,0 +1,59 @@
+/* @bruin
+
+materialization:
+  type: table
+
+columns:
+  - name: id
+    type: int64
+    description: Just a number
+    primary_key: true
+    checks:
+      - name: not_null
+      - name: positive
+      - name: non_negative
+  - name: country
+    type: string
+    description: the country
+    primary_key: true
+    checks:
+      - name: not_null
+  - name: name
+    type: string
+    description: Just a name
+    update_on_merge: true
+    checks:
+      - name: unique
+      - name: not_null
+
+@bruin */
+
+SELECT
+    1 AS id,
+    'spain' AS country,
+    'alberto' AS name
+UNION ALL
+SELECT
+    2 AS id,
+    'germany' AS country,
+    'frank' AS name
+UNION ALL
+SELECT
+    3 AS id,
+    'germany' AS country,
+    'franz' AS name
+UNION ALL
+SELECT
+    4 AS id,
+    'france' AS country,
+    'jean' AS name
+UNION ALL
+SELECT
+    5 AS id,
+    'poland' AS country,
+    'maciej' AS name
+UNION ALL
+SELECT
+    6 AS id,
+    'india' AS country,
+    'yuvraj' AS name
---
+++ templates/bigquery/macros/transformations.sql
@@ -0,0 +1,37 @@
+{# Common data transformation patterns for BigQuery #}
+
+{% macro pivot_sum(table, row_column, pivot_column, value_column, pivot_values) -%}
+SELECT
+    {{ row_column }},
+    {%- for val in pivot_values %}
+    SUM(CASE WHEN {{ pivot_column }} = '{{ val }}' THEN {{ value_column }} ELSE 0 END) as {{ val | replace(' ', '_') | replace('-', '_') }}
+    {%- if not loop.last %},{% endif %}
+    {%- endfor %}
+FROM {{ table }}
+GROUP BY {{ row_column }}
+{%- endmacro %}
+
+{% macro deduplicate(table, partition_column, order_column) -%}
+SELECT * EXCEPT(rn) FROM (
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY {{ partition_column }}
+               ORDER BY {{ order_column }} DESC
+           ) as rn
+    FROM {{ table }}
+)
+WHERE rn = 1
+{%- endmacro %}
+
+{% macro generate_surrogate_key(columns) -%}
+TO_HEX(MD5(CONCAT(
+    {%- for col in columns %}
+        CAST({{ col }} AS STRING)
+        {%- if not loop.last %}, '||', {% endif %}
+    {%- endfor %}
+))) as surrogate_key
+{%- endmacro %}
+
+{% macro safe_divide(numerator, denominator, default=0) -%}
+SAFE_DIVIDE({{ numerator }}, {{ denominator }})
+{%- endmacro %}
