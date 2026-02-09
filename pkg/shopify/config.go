c5310994ab5737ecc5476fabfc675d5919087358
mustafa3rsan <mustafa3rsan@users.noreply.github.com>
2026-02-09 17:36:55 +0300

    new-params

--- pkg/shopify/config.go
+++ pkg/shopify/config.go
@@ -5,10 +5,19 @@ import (
 )
 
 type Config struct {
-	URL    string
-	APIKey string
+	URL          string
+	APIKey       string
+	ClientID     string
+	ClientSecret string
 }
 
 func (c *Config) GetIngestrURI() string {
-	return fmt.Sprintf("shopify://%s?api_key=%s", c.URL, c.APIKey)
+	uri := fmt.Sprintf("shopify://%s?api_key=%s", c.URL, c.APIKey)
+	if c.ClientID != "" {
+		uri += fmt.Sprintf("&client_id=%s", c.ClientID)
+	}
+	if c.ClientSecret != "" {
+		uri += fmt.Sprintf("&client_secret=%s", c.ClientSecret)
+	}
+	return uri
 }
