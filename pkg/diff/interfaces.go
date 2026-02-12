b2c138225f5a1c2d868a147e549238e682c037f5
Djamila Baroudi <baroudi.d7@gmail.com>
2026-02-12 14:09:39 +0100

    add cost estimation interfaces and data structures for table diff calculations

--- pkg/diff/interfaces.go
+++ pkg/diff/interfaces.go
@@ -8,3 +8,30 @@ import (
 type TableSummarizer interface {
 	GetTableSummary(ctx context.Context, tableName string, schemaOnly bool) (*TableSummaryResult, error)
 }
+
+type CostEstimator interface {
+	EstimateTableDiffCost(ctx context.Context, tableName string, schemaOnly bool) (*TableDiffCostEstimate, error)
+}
+
+type QueryCostEstimate struct {
+	QueryType      string `json:"queryType"`      
+	Query          string `json:"query"`          
+	BytesProcessed int64  `json:"bytesProcessed"` 
+	BytesBilled    int64  `json:"bytesBilled"`   
+}
+
+type TableDiffCostEstimate struct {
+	TableName           string               `json:"tableName"`
+	Queries             []*QueryCostEstimate `json:"queries"`
+	TotalBytesProcessed int64                `json:"totalBytesProcessed"`
+	TotalBytesBilled    int64                `json:"totalBytesBilled"`
+	Supported           bool                 `json:"supported"`
+	UnsupportedReason   string               `json:"unsupportedReason,omitempty"`
+}
+
+type DiffCostEstimate struct {
+	SourceTable         *TableDiffCostEstimate `json:"sourceTable"`
+	TargetTable         *TableDiffCostEstimate `json:"targetTable"`
+	TotalBytesProcessed int64                  `json:"totalBytesProcessed"`
+	TotalBytesBilled    int64                  `json:"totalBytesBilled"`
+}
