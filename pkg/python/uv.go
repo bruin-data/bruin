d4a7a7fe9a7dfcfe126fb121767ef95c8b5efdaf
iremcaginyurtturk <cagin.yurtturk@getbruin.com>
2026-06-08 17:52:04 +0300

    bump ingestr v1.0.22

--- pkg/python/uv.go
+++ pkg/python/uv.go
@@ -43,7 +43,7 @@ const (
 	// IngestrVersionV0 is the legacy ingestr release pinned for parameters.version=v0.
 	IngestrVersionV0 = "0.14.155"
 	// IngestrVersionV1 is the current ingestr release used by default and for parameters.version=v1.
-	IngestrVersionV1 = "1.0.20"
+	IngestrVersionV1 = "1.0.22"
 	sqlfluffVersion  = "3.4.1"
 )
 
