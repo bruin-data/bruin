603c18d7e4a0f706e471508a1e1859ee66c0de4b
Tanay Bensu Yurtturk <bensu.yurtturk@getbruin.com>
2026-07-01 10:00:25 +0300

    lint

--- pkg/gsheets/config_test.go
+++ pkg/gsheets/config_test.go
@@ -26,7 +26,6 @@ func TestGetIngestrURI(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			if got := tt.config.GetIngestrURI(); got != tt.want {
