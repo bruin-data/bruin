df29b209936d291a52e3c89de807b7ba38f9ae2a
Burak Karakan <burak.karakan@gmail.com>
2026-07-08 17:43:25 +0300

    Fix T-SQL surrogate key varchar casts

--- pkg/mssql/bruin_funcs.go
+++ pkg/mssql/bruin_funcs.go
@@ -7,7 +7,7 @@ import (
 
 func init() { //nolint:gochecknoinits
 	jinja.RegisterPlatformOverrides(jinja.PlatformMSSQL, jinja.MergeBuiltinOverrides(jinja.TSQLURLHelpers(), map[string]any{
-		"generate_surrogate_key": jinja.SurrogateKeyWith("varchar", ansisql.HashBytesHashFn),
+		"generate_surrogate_key": jinja.SurrogateKeyWith("varchar(max)", ansisql.HashBytesHashFn),
 		"deduplicate":            ansisql.DeduplicateSubquery,
 		"pivot":                  jinja.PivotWithIdentifierQuote(jinja.BracketQuoteIdentifier),
 		"width_bucket":           jinja.TSQLWidthBucket,
---
+++ pkg/synapse/bruin_funcs_test.go
@@ -0,0 +1,27 @@
+package synapse
+
+import (
+	"testing"
+
+	"github.com/bruin-data/bruin/pkg/jinja"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSynapseBuiltinOverrides(t *testing.T) {
+	t.Parallel()
+
+	renderer := jinja.NewRenderer(jinja.Context{
+		"bruin": jinja.BuiltinFunctions(jinja.PlatformSynapse),
+	})
+
+	t.Run("surrogate_key casts to unbounded varchar", func(t *testing.T) {
+		t.Parallel()
+		result, err := renderer.Render("{{ bruin.generate_surrogate_key(['user_id', 'session_id']) }}")
+		require.NoError(t, err)
+		assert.Contains(t, result, "cast(user_id as varchar(max))")
+		assert.Contains(t, result, "cast(session_id as varchar(max))")
+		assert.NotContains(t, result, "cast(user_id as varchar)")
+		assert.Contains(t, result, "hashbytes('md5',")
+	})
+}
